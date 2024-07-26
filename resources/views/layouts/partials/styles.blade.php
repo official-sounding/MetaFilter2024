e20725898a6ba961ccdc190c36a32c63bd1ccc5a
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-07-26 11:24:55 -0700

    Added Bulma CDN

--- resources/views/layouts/partials/styles.blade.php
+++ resources/views/layouts/partials/styles.blade.php
@@ -1,16 +1,18 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=montserrat:400|source-sans-pro:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
 
-<style>
-    [x-cloak] {
-        display: none !important;
-    }
-</style>
-
-@vite($stylesheets)
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.1/css/bulma.min.css">
 
 @if (isset($useLivewire) && $useLivewire === true)
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+    </style>
+
     @livewireStyles
 @endif
+
+@vite($stylesheets)
 
 @stack('styles')
