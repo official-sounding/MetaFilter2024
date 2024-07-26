2eed016b617a4f11e5a468a4753681e295f69532
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-07-26 12:14:42 -0700

    Site title h1 on home pages

--- resources/views/layouts/partials/site-title.blade.php
+++ resources/views/layouts/partials/site-title.blade.php
@@ -1,8 +1,15 @@
 <a href="{{ route($subsite['route']) }}" title="Home page">
     <hgroup class="site-title">
-        <h1 class="site-title-text">
-            <span class="white">{{ $subsite['whiteText'] }}</span><span class="green">{{ $subsite['greenText'] }}</span>
-        </h1>
+        @if (empty(request()->segment(1)))
+            <h1 class="site-title-text">
+                <span class="white">{{ $subsite['whiteText'] }}</span><span class="green">{{ $subsite['greenText'] }}</span>
+            </h1>
+        @else
+            <p class="site-title-text">
+                <span class="white">{{ $subsite['whiteText'] }}</span><span class="green">{{ $subsite['greenText'] }}</span>
+            </p>
+        @endif
+
         @if ($subsite['tagline'] !== '')
             <p class="tagline">{{ $subsite['tagline'] }}</p>
         @endif
