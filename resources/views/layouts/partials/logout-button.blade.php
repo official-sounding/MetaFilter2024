90992eea491f9a9d68f30ebeb1861f328fb59b48
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-07-27 10:23:47 -0700

    Deduped login form

--- resources/views/layouts/partials/logout-button.blade.php
+++
@@ -1,9 +0,0 @@
-<form method="POST" class="logout-form" action="{{ route('logout') }}">
-    @csrf
-
-    <button type="submit" class="logout-button">
-        <span class="icon-logout">
-            Log Out
-        </span>
-    </button>
-</form>
