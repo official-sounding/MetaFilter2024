76414e63e5db5d821526525369ccfe4303dd03c5
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-07-27 10:24:33 -0700

    Split bunny CSS into two lines

--- resources/views/layouts/partials/styles.blade.php
+++ resources/views/layouts/partials/styles.blade.php
@@ -1,5 +1,6 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=montserrat:400|source-sans-pro:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.bunny.net/css?family=montserrat:400">
+<link rel="stylesheet" href="https://fonts.bunny.net/css?source-sans-pro:300,300i,400,400i,600,600i,700,700i">
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.1/css/bulma.min.css">
 
