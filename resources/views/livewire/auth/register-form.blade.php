f7f57df50ea5d721c84d78a5a6e02783ce04ea71
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-07-27 22:13:00 -0700

    Changed public to public_html

--- resources/views/livewire/auth/register-form.blade.php
+++ resources/views/livewire/auth/register-form.blade.php
@@ -1,15 +1,15 @@
 @php
     $emailNote = <<<EOT
     Please use a working address. We won&rsquo;t spam you, and we&rsquo;ll <strong>never</strong> give away your address to anyone.
-    By default this address is hidden from users and the public, but in you can choose to show it publicly. [Privacy policy]
+    By default this address is hidden from users and the public_html, but in you can choose to show it publicly. [Privacy policy]
     EOT;
 
     $nameNote = <<<EOT
-    If you enter a name here, it&rsquo;s shown on your public profile page, but it&rsquo;s fine to leave this blank.
+    If you enter a name here, it&rsquo;s shown on your public_html profile page, but it&rsquo;s fine to leave this blank.
     EOT;
 
     $homepageUrlNote = <<<EOT
-    If you enter a URL here, it&rsquo;s shown on your public profile page, but it&rsquo;s fine to leave this blank.
+    If you enter a URL here, it&rsquo;s shown on your public_html profile page, but it&rsquo;s fine to leave this blank.
     EOT;
 @endphp
 
