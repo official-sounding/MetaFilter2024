5ef45e35bec02dde5d9625a4e0477a1a426ceb1d
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-07-27 19:15:49 -0700

    Moved auth routes to metafilter subdomain

--- routes/auth.php
+++ routes/auth.php
@@ -15,52 +15,60 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name(RouteNameEnum::AUTH_REGISTER_CREATE->value);
+$appUrl = config('app.url');
 
-    Route::post('register', [RegisteredUserController::class, 'store'])
-        ->name(RouteNameEnum::AUTH_REGISTER_STORE->value);
+Route::domain('www.' . $appUrl)
+    ->middleware('guest')->group(
+        function () {
+            Route::get('register', [RegisteredUserController::class, 'create'])
+                ->name(RouteNameEnum::AUTH_REGISTER_CREATE->value);
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name(RouteNameEnum::AUTH_LOGIN_CREATE->value);
+            Route::post('register', [RegisteredUserController::class, 'store'])
+                ->name(RouteNameEnum::AUTH_REGISTER_STORE->value);
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store'])
-        ->name(RouteNameEnum::AUTH_LOGIN_STORE->value);
+            Route::get('login', [AuthenticatedSessionController::class, 'create'])
+                ->name(RouteNameEnum::AUTH_LOGIN_CREATE->value);
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-        ->name(RouteNameEnum::AUTH_FORGOT_PASSWORD_CREATE->value);
+            Route::post('login', [AuthenticatedSessionController::class, 'store'])
+                ->name(RouteNameEnum::AUTH_LOGIN_STORE->value);
 
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->name(RouteNameEnum::AUTH_FORGOT_PASSWORD_STORE->value);
+            Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+                ->name(RouteNameEnum::AUTH_FORGOT_PASSWORD_CREATE->value);
 
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-        ->name(RouteNameEnum::AUTH_RESET_PASSWORD_CREATE->value);
+            Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+                ->name(RouteNameEnum::AUTH_FORGOT_PASSWORD_STORE->value);
 
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name(RouteNameEnum::AUTH_RESET_PASSWORD_STORE->value);
-});
+            Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+                ->name(RouteNameEnum::AUTH_RESET_PASSWORD_CREATE->value);
 
-Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
-        ->name('verification.notice');
+            Route::post('reset-password', [NewPasswordController::class, 'store'])
+                ->name(RouteNameEnum::AUTH_RESET_PASSWORD_STORE->value);
+        },
+    );
 
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-        ->middleware(['signed', 'throttle:6,1'])
-        ->name('verification.verify');
+Route::domain('www.' . $appUrl)
+    ->middleware('auth')->group(
+        function () {
+            Route::get('verify-email', EmailVerificationPromptController::class)
+                ->name('verification.notice');
 
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware('throttle:6,1')
-        ->name('verification.send');
+            Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+                ->middleware(['signed', 'throttle:6,1'])
+                ->name('verification.verify');
 
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->name('password.confirm');
+            Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+                ->middleware('throttle:6,1')
+                ->name('verification.send');
 
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+            Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+                ->name('password.confirm');
 
-    Route::put('password', [PasswordController::class, 'update'])
-        ->name('password.update');
+            Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-        ->name(RouteNameEnum::AUTH_LOGOUT->value);
-});
+            Route::put('password', [PasswordController::class, 'update'])
+                ->name('password.update');
+
+            Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+                ->name(RouteNameEnum::AUTH_LOGOUT->value);
+        },
+    );
