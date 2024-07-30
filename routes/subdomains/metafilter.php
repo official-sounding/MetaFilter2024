5d0178b949d8ea4decd146437e9c5c6c86e3ff08
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-07-30 08:43:12 -0700

    Added MetaFilter routes

--- routes/subdomains/metafilter.php
+++ routes/subdomains/metafilter.php
@@ -3,9 +3,11 @@
 declare(strict_types=1);
 
 use App\Enums\RouteNameEnum;
+use App\Http\Controllers\ArchivesController;
 use App\Http\Controllers\ContactMessageController;
 use App\Http\Controllers\FundingController;
 use App\Http\Controllers\MailController;
+use App\Http\Controllers\PopularPostController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TagController;
@@ -26,17 +28,26 @@ Route::middleware('auth')->group(function () {
 Route::get('about', [ContactMessageController::class, 'create'])
     ->name('metafilter.about.index');
 
-Route::get('mail', [MailController::class, 'index'])
-    ->name('metafilter.mail.index');
-
-Route::get('tags', [TagController::class, 'index'])
-    ->name('metafilter.tags.index');
+Route::get('archives', [ArchivesController::class, 'index'])
+    ->name(RouteNameEnum::POST_ARCHIVES_INDEX->value);
 
 Route::get('contact', [ContactMessageController::class, 'create'])
     ->name('metafilter.contact-message.create');
 
 Route::get('funding', [FundingController::class, 'index'])
     ->name(RouteNameEnum::METAFILTER_FUNDING_INDEX->value);
+
+Route::get('mail', [MailController::class, 'index'])
+    ->name('metafilter.mail.index');
+
+Route::get('popular', [PopularPostController::class, 'index'])
+    ->name(RouteNameEnum::METAFILTER_POPULAR_POST_INDEX->value);
+
+Route::get('random', [PopularPostController::class, 'index'])
+    ->name(RouteNameEnum::METAFILTER_RANDOM_POST_SHOW->value);
+
+Route::get('tags', [TagController::class, 'index'])
+    ->name('metafilter.tags.index');
 
 Route::controller(PostController::class)->group(function () {
     Route::get('', 'index')
@@ -45,6 +56,6 @@ Route::controller(PostController::class)->group(function () {
     Route::get('create', 'create')
         ->name(RouteNameEnum::METAFILTER_POST_CREATE->value);
 
-    Route::get('{post}', 'show')
+    Route::get('{post}/{slug}', 'show')
         ->name(RouteNameEnum::METAFILTER_POST_SHOW->value);
 });
