5e12c1408f51e63b1060327670c868f5308f49e2
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-07-30 14:57:15 -0700

    Installed spatie/laravel-activitylog

---
+++ database/migrations/2024_07_30_215440_add_batch_uuid_column_to_activity_log_table.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::connection(config('activitylog.database_connection'))
+            ->table(config('activitylog.table_name'), function (Blueprint $table) {
+                $table->uuid('batch_uuid')
+                    ->nullable()
+                    ->after('properties');
+            });
+    }
+
+    public function down(): void
+    {
+        Schema::connection(config('activitylog.database_connection'))
+            ->table(config('activitylog.table_name'), function (Blueprint $table) {
+                $table->dropColumn('batch_uuid');
+            });
+    }
+};
