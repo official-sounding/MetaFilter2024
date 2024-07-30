50ae47386e8888092cad10cd0866ab998e7cb6e4
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-07-30 08:45:01 -0700

    Stubbed out event listeners

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,16 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+document.addEventListener('click', function (event) {
+
+    // If the clicked element doesn't have the right selector, bail
+    if (!event.target.matches('.click-me')) return;
+
+    // Don't follow the link
+    event.preventDefault();
+
+    // Log the clicked element in the console
+    console.log(event.target);
+
+}, false);
