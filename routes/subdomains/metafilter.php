b43b035bf5601f836f25d82818247a6a37c11156
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-08-16 10:27:11 -0700

    Route updates

--- routes/subdomains/metafilter.php
+++ routes/subdomains/metafilter.php
@@ -5,23 +5,34 @@ declare(strict_types=1);
 use App\Enums\RouteNameEnum;
 use App\Http\Controllers\ArchivesController;
 use App\Http\Controllers\ContactMessageController;
+use App\Http\Controllers\FavoritesController;
 use App\Http\Controllers\FundingController;
 use App\Http\Controllers\MailController;
 use App\Http\Controllers\PopularPostController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PreferencesController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
+    Route::get('favorites', [FavoritesController::class, 'index'])
+        ->name(RouteNameEnum::FAVORITES_INDEX->value);
+
+    Route::get('preferences/{user}', [PreferencesController::class, 'edit'])
+        ->name(RouteNameEnum::PREFERENCES_EDIT->value);
+
+    Route::get('profile/{user:id}', [ProfileController::class, 'show'])
+        ->name(RouteNameEnum::PROFILE_SHOW->value);
+
     Route::get('profile', [ProfileController::class, 'edit'])
-        ->name('profile.edit');
+       ->name(RouteNameEnum::PROFILE_EDIT->value);
 
     Route::patch('profile', [ProfileController::class, 'update'])
-        ->name('profile.update');
+        ->name(RouteNameEnum::PROFILE_UPDATE->value);
 
     Route::delete('profile', [ProfileController::class, 'delete'])
-        ->name('profile.delete');
+        ->name(RouteNameEnum::PROFILE_DELETE->value);
 });
 
 // TODO: use PageController
