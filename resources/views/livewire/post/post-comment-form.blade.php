664a1845ed3f285498d14b0a4d664643aabd937b
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-09-01 17:17:14 -0700

    Fixed wysiwyg

--- resources/views/livewire/post/post-comment-form.blade.php
+++ resources/views/livewire/post/post-comment-form.blade.php
@@ -1,29 +1,36 @@
-
 <form wire:submit="save()">
     @include('forms.partials.validation-summary')
 
     <small>posting as
         <a title="{{ __('View profile') }}"
-            href="{{ route($profileRoute, [
+           href="{{ route($profileRoute, [
             'user' => auth()->user()
         ]) }}">
             <stong>{{ auth()->user()->username }}</stong>
         </a>
     </small>
 
-
     <div wire:ignore>
-        <textarea
-            wire:model.live="contents"
-            name="contents"
-            id="wysiwyg">
-            {!! $contents !!}
-        </textarea>
-
-        Livewire contents property : {{ $contents }}
+        <textarea wire:model.live="contents" name="contents" id="contents"></textarea>
     </div>
 
     <button type="submit">
         {{ __('Add Comment') }}
     </button>
 </form>
+
+@push('scripts')
+    <script src="https://cdn.ckeditor.com/ckeditor5/27.1.0/classic/ckeditor.js"></script>
+    <script>
+        ClassicEditor
+            .create(document.querySelector('#contents'))
+            .then(editor => {
+                editor.model.document.on('change:data', () => {
+                    @this.set('contents', editor.getData());
+                })
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    </script>
+@endpush
