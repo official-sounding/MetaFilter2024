311edce1e6353068bdc852b1c5e3fefbf807bf69
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-09-02 18:09:52 -0700

    Added LivewireServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Providers\AppServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
 use App\Providers\HorizonServiceProvider;
+use App\Providers\LivewireServiceProvider;
 use App\Providers\RepositoryServiceProvider;
 use App\Providers\TelescopeServiceProvider;
 use App\Providers\ViewComposerServiceProvider;
@@ -13,6 +14,7 @@ return [
     AdminPanelProvider::class,
     AppServiceProvider::class,
     HorizonServiceProvider::class,
+    LivewireServiceProvider::class,
     RepositoryServiceProvider::class,
     TelescopeServiceProvider::class,
     ViewComposerServiceProvider::class,
