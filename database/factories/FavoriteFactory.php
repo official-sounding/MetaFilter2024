ea93b1484ca35c619ad7cc765e5b166ace62400c
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-09-28 15:08:19 -0700

    Added factories

---
+++ database/factories/FavoriteFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Favorite;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+final class FavoriteFactory extends Factory
+{
+    protected $model = Favorite::class;
+
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/FlagFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Flag;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+final class FlagFactory extends Factory
+{
+    protected $model = Flag::class;
+
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
