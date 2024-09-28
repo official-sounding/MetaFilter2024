b540879dc33463310185ed940bb74741a6175ee2
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-09-28 15:06:54 -0700

    Model updates

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Mpociot\Versionable\VersionableTrait;
 use Spatie\Activitylog\LogOptions;
