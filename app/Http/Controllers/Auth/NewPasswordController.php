49358667d9f17be713738032680469e645665445
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-09-28 15:06:42 -0700

    Code cleanup

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -29,7 +29,7 @@ final class NewPasswordController extends BaseAuthController
 
         // Here we will attempt to reset the user's password. If it is successful we
         // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
+        // database. Otherwise, we will parse the error and return the response.
         $status = PasswordFacade::reset(
             $request->only([
                 'email',
