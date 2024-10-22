efc9d4a29356f3de121569c9b6d787476d1b223e
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-10-22 00:08:35 -0700

    View composer updates

---
+++ app/View/Composers/Navigation/CreatePostButtonViewComposer.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\View\Composers\Navigation;
+
+use App\Traits\NavigationTrait;
+use App\Traits\SubsiteTrait;
+use App\View\Composers\ViewComposerInterface;
+use Illuminate\Contracts\View\View;
+
+final class CreatePostButtonViewComposer implements ViewComposerInterface
+{
+    use NavigationTrait;
+    use SubsiteTrait;
+
+    protected string $subdomain;
+
+    public const string DEFAULT_TEXT = 'Create Post';
+
+    public function __construct()
+    {
+        $this->subdomain = $this->getSubdomainFromUrl();
+    }
+
+    public function compose(View $view): void
+    {
+        $createPost = '';
+
+        $items = config("metafilter.navigation.create-post-links.$this->subdomain");
+
+        if ($items === null) {
+            $this->logError('Create post items are null.');
+        } else {
+            foreach ($items as $item) {
+                if (!isset($item['text'])) {
+                    $item['text'] = self::DEFAULT_TEXT;
+                }
+
+                $createPost .= $this->getNavigationItem($item);
+            }
+        }
+
+        $view->with([
+            'defaultText' => self::DEFAULT_TEXT,
+            'createPost' => $createPost,
+            'items' => $items,
+        ]);
+    }
+}
---
+++ app/View/Composers/Navigation/PrimarySidebarNavigationComposer.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\View\Composers\Navigation;
+
+use App\Enums\RouteNameEnum;
+use App\Enums\SubsiteEnum;
+use App\Traits\LoggingTrait;
+use App\Traits\NavigationTrait;
+use App\Traits\SubsiteTrait;
+use App\View\Composers\ViewComposerInterface;
+use Illuminate\Contracts\View\View;
+
+final class PrimarySidebarNavigationComposer implements ViewComposerInterface
+{
+    use LoggingTrait;
+    use NavigationTrait;
+    use SubsiteTrait;
+
+    protected string $subdomain;
+
+    public function __construct()
+    {
+        $this->subdomain = $this->getSubdomainFromUrl();
+    }
+
+    public function compose(View $view): void
+    {
+        $navigation = '<nav>';
+        $navigation .= '<ul>';
+
+        $items = config("metafilter.navigation.primary-sidebar-navigation.$this->subdomain");
+
+        if ($items === null) {
+            $this->logError('Primary sidebar navigation items are null.');
+        } else {
+            foreach ($items as $item) {
+                $navigation .= $this->getNavigationItem($item);
+            }
+        }
+
+        $navigation .= '</ul>';
+        $navigation .= '</nav>';
+
+        $view->with('primarySidebarNavigation', $navigation);
+    }
+}
