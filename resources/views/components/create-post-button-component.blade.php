11e4811a849649578be33483effba98810101fc2
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-10-22 00:07:18 -0700

    View updates

---
+++ resources/views/components/create-post-button-component.blade.php
@@ -0,0 +1,13 @@
+@if ($links->count() > 1)
+    <button class="button">
+    </button>
+@else
+    <a class="button" href="{{ route($links[0]['route']) }}">
+        @if (isset($links[0]['text']))
+            {{ $links[0]['text'] }}
+        @else
+            {{ $defaultCreatePostText }}
+        @endif
+    </a>
+@endif
+
