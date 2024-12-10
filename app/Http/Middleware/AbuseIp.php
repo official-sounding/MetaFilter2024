c0b9598805521cef9f4589b02fcdde58d2589bc2
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-12-09 17:52:27 -0800

    Middleware updates

---
+++ app/Http/Middleware/AbuseIp.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AbuseIp
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $abuseip = config('abuseip.abuse_ips')();
+
+        if (in_array($request->ip(), $abuseip)) {
+
+            // Log::info('Blocking IP: ' . $request->ip());
+            return response('Your IP address has been blocked', 403);
+        }
+
+        return $next($request);
+    }
+}
