f9ed78e6e445d9efcc78dbd7a58ab0e3ce8c043c
kirkaracha <3633070+kirkaracha@users.noreply.github.com>
2024-12-28 15:54:30 -0800

    Doc cleanup

---
+++ larecipe-components/Docs/src/ThemeServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Metafilter\Docs;
+
+use BinaryTorch\LaRecipe\LaRecipe;
+use Illuminate\Support\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        LaRecipe::style('docs', __DIR__ . '/../resources/css/theme.css');
+    }
+
+    public function register(): void
+    {
+        //
+    }
+}
