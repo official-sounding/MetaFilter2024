cf856fde9f64816e4da66976a3f54b0214cdf735
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-09 14:40:44 -0800

    Updated request to check for available languages

--- app/Http/Requests/StoreLanguageRequest.php
+++ app/Http/Requests/StoreLanguageRequest.php
@@ -4,20 +4,25 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 final class StoreLanguageRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     public function rules(): array
     {
+        $locales = config('app.available_locales');
+
         return [
-            //
+            'language' => [
+                'required',
+                'string',
+                "in_array:$locales.*",
+            ],
         ];
     }
 }
