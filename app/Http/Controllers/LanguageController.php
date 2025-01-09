dd90c903d9b53c1bbdd38a819b19ba9342c88483
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-09 14:41:00 -0800

    Initial coding

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -5,14 +5,23 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreLanguageRequest;
+use App\Traits\LoggingTrait;
 
 final class LanguageController extends BaseController
 {
+    use LoggingTrait;
+
     public function store(StoreLanguageRequest $request)
     {
+        $availableLocales = config('app.available_locales');
+
         $language = $request->input('language');
 
-        session(['language' => $language]);
+        if (in_array($language, $availableLocales)) {
+            session(['language' => $language]);
+        } else {
+            $this->logError('Invalid language selected: ' . $language);
+        }
 
         return redirect()->back();
     }
