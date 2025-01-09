e1f578af5fe7a9deb60bec0bc949400c5627c14a
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-08 19:25:29 -0800

    Added auth actions

---
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Fortify;
+
+use Illuminate\Validation\Rules\Password;
+
+trait PasswordValidationRules
+{
+    protected function passwordRules(): array
+    {
+        return [
+            'required',
+            'string',
+            Password::default(),
+            'confirmed',
+        ];
+    }
+}
---
+++ app/Actions/Fortify/UpdateUserPassword.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Contracts\UpdatesUserPasswords;
+
+final class UpdateUserPassword implements UpdatesUserPasswords
+{
+    use PasswordValidationRules;
+
+    public function update(User $user, array $input): void
+    {
+        Validator::make($input, [
+            'current_password' => ['required', 'string', 'current_password:web'],
+            'password' => $this->passwordRules(),
+        ], [
+            'current_password.current_password' => trans('The provided password does not match your current password.'),
+        ])->validateWithBag('updatePassword');
+
+        $user->forceFill([
+            'password' => Hash::make($input['password']),
+        ])->save();
+    }
+}
