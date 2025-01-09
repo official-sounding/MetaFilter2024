2bc5c1b49539f4d29aeffab04fa43c06d3a43ad9
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-08 19:28:42 -0800

    Subscription updates

--- app/Subscriptions/PayPalSubscriptions.php
+++ app/Subscriptions/PayPalSubscriptions.php
@@ -1,7 +1,49 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Subscriptions;
 
-class PayPalSubscriptions {
+use App\Traits\LoggingTrait;
+use Exception;
+use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
+final class PayPalSubscriptions implements SubscriptionInterface
+{
+    use LoggingTrait;
+
+    protected PayPalClient $provider;
+
+    public function __construct()
+    {
+        $this->provider = new PayPalClient();
+
+        try {
+            $this->provider->setApiCredentials(config('paypal'));
+        } catch (Exception $exception) {
+            $this->logError($exception);
+        }
+    }
+
+    public function create(int $plan_id, int $coupon_user_id, string $method, float $amount = 0)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function cancel(string $subscription_id = null)
+    {
+        // TODO: Implement cancel() method.
+    }
+
+    public function pause()
+    {
+        // TODO: Implement pause() method.
+    }
+
+    public function resume()
+    {
+        // TODO: Implement resume() method.
+    }
 }
+
+// https://medium.com/@haziqali516/simplifying-paypal-subscriptions-with-laravel-a-practical-guide-106be0755f32
--- app/Subscriptions/SubscriptionInterface.php
+++ app/Subscriptions/SubscriptionInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Subscriptions;
 
-interface SubscriptionsInterface
+interface SubscriptionInterface
 {
     public function create(int $plan_id, int $coupon_user_id, string $method, float $amount = 0);
     public function cancel(string $subscription_id = null);
