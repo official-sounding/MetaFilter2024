15032dcea83d558535c4c29885f688c875149926
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-10 12:53:36 -0800

    Added chunking for better performance

--- database/seeders/Development/FakeCommentSeeder.php
+++ database/seeders/Development/FakeCommentSeeder.php
@@ -6,13 +6,21 @@ namespace Database\Seeders\Development;
 
 use App\Models\Comment;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 final class FakeCommentSeeder extends Seeder
 {
-    private const int NUMBER_OF_FAKE_COMMENTS = 1000;
+    private const int CHUNK_SIZE = 100;
+    private const int NUMBER_OF_FAKE_COMMENTS = 5000;
 
     public function run(): void
     {
-        Comment::factory(self::NUMBER_OF_FAKE_COMMENTS)->create();
+        DB::connection()->disableQueryLog();
+
+        $comments = Comment::factory()->count(self::NUMBER_OF_FAKE_COMMENTS)->make()->toArray();
+
+        collect($comments)->chunk(self::CHUNK_SIZE)->each(function ($chunk) {
+            Comment::insert($chunk->toArray());
+        });
     }
 }
--- database/seeders/Development/FakePostSeeder.php
+++ database/seeders/Development/FakePostSeeder.php
@@ -6,13 +6,21 @@ namespace Database\Seeders\Development;
 
 use App\Models\Post;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 final class FakePostSeeder extends Seeder
 {
-    private const int NUMBER_OF_FAKE_POSTS = 1500;
+    private const int CHUNK_SIZE = 100;
+    private const int NUMBER_OF_FAKE_POSTS = 2000;
 
     public function run(): void
     {
-        Post::factory(self::NUMBER_OF_FAKE_POSTS)->create();
+        DB::connection()->disableQueryLog();
+
+        $posts = Post::factory()->count(self::NUMBER_OF_FAKE_POSTS)->make()->toArray();
+
+        collect($posts)->chunk(self::CHUNK_SIZE)->each(function ($chunk) {
+            Post::insert($chunk->toArray());
+        });
     }
 }
