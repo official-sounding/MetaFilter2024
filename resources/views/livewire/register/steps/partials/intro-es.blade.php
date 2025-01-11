8045aec9f52d53635d7e1a2b607a07684aaed35b
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-10 18:20:10 -0800

    Moved locale text to partials

---
+++ resources/views/livewire/register/steps/partials/intro-es.blade.php
@@ -0,0 +1,34 @@
+<h2>Quienes somos</h2>
+
+<p>
+    MetaFilter es una comunidad pública con tres pilares: compartir y hablar sobre &ldquo;cosas encontradas en Internet&rdquo;
+    sobre diversos temas, hacer y responder preguntas y debatir sobre medios populares.
+    Nos esforzamos por ser una zona amigable e inclusiva, libre de acoso.
+</p>
+
+<p>¡Esperamos que te unas a nosotros!</p>
+
+<h2>Tarifa de inscripción única de $5</h2>
+
+<p>
+    Hay un cargo único de US$5 para crear una cuenta en MetaFilter, para desalentar a los spammers.
+    Una vez que complete el formulario de registro, será dirigido a PayPal para finalizar el proceso.
+</p>
+
+<h2>Qué debes saber antes de registrarte</h2>
+
+<ul>
+    <li>
+        Todas las publicaciones y comentarios son visibles tanto para los usuarios registrados como para
+        el público, y muestran su nombre de usuario.
+    </li>
+    <li>Su nombre de usuario no se puede cambiar una vez creada su cuenta.</li>
+    <li>
+        No se permiten promociones, incluidas autopromociones o enlaces (excepto en su perfil), SEO, relaciones públicas, marketing o publicidad.
+        @include('livewire.register.steps.partials.banning-venn-diagram')
+    </li>
+</ul>
+
+<button class="button primary-button" wire:click="nextStep">
+    ¿Listo para registrarte? ¡Vamos allá!
+</button>
