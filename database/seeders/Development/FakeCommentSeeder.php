d2f306fad514b9b302cfc2633c7fe4787c9eba3e
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-11 14:18:46 -0800

    Reverted to creating all entries

--- database/seeders/Development/FakeCommentSeeder.php
+++ database/seeders/Development/FakeCommentSeeder.php
@@ -10,17 +10,12 @@ use Illuminate\Support\Facades\DB;
 
 final class FakeCommentSeeder extends Seeder
 {
-    private const int CHUNK_SIZE = 100;
     private const int NUMBER_OF_FAKE_COMMENTS = 5000;
 
     public function run(): void
     {
         DB::connection()->disableQueryLog();
 
-        $comments = Comment::factory()->count(self::NUMBER_OF_FAKE_COMMENTS)->make()->toArray();
-
-        collect($comments)->chunk(self::CHUNK_SIZE)->each(function ($chunk) {
-            Comment::insert($chunk->toArray());
-        });
+        Comment::factory(self::NUMBER_OF_FAKE_COMMENTS)->create();
     }
 }
--- database/seeders/Development/FakePostSeeder.php
+++ database/seeders/Development/FakePostSeeder.php
@@ -10,17 +10,12 @@ use Illuminate\Support\Facades\DB;
 
 final class FakePostSeeder extends Seeder
 {
-    private const int CHUNK_SIZE = 100;
-    private const int NUMBER_OF_FAKE_POSTS = 2000;
+    private const int NUMBER_OF_FAKE_POSTS = 2500;
 
     public function run(): void
     {
         DB::connection()->disableQueryLog();
 
-        $posts = Post::factory()->count(self::NUMBER_OF_FAKE_POSTS)->make()->toArray();
-
-        collect($posts)->chunk(self::CHUNK_SIZE)->each(function ($chunk) {
-            Post::insert($chunk->toArray());
-        });
+        Post::factory(self::NUMBER_OF_FAKE_POSTS)->create();
     }
 }
