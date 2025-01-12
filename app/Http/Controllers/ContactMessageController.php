7f952f764886433d50377011184c2775f4b1bb93
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-12 10:39:18 -0800

    Replaced ContactController with ContactMessageController

---
+++ app/Http/Controllers/ContactMessageController.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Contact\StoreContactMessageRequest;
+use App\Services\ContactMessageService;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
+final class ContactMessageController extends BaseController
+{
+    public function __construct(
+        protected ContactMessageService $contactMessageService,
+    ) {
+        parent::__construct();
+    }
+
+    public function create(): View
+    {
+        return view('contact.create', [
+            'title' => trans('Contact Us'),
+        ]);
+    }
+
+    public function store(StoreContactMessageRequest $request): RedirectResponse
+    {
+        $stored = $this->contactMessageService->store($request->validated());
+
+        if ($stored) {
+            $status = trans('Your message has been sent successfully.');
+
+            return redirect()
+                ->route('contact.thanks')
+                ->with('status', $status);
+        } else {
+            $status = 'Sorry, there was a problem sending your message. Please try again.';
+
+            return back()
+                ->withInput()
+                ->with('status', $status);
+        }
+    }
+
+    public function thanks(): View
+    {
+        return view('contact.thanks', [
+            'title' => trans('Contact Us'),
+        ]);
+    }
+}
