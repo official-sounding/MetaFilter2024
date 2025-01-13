3f46fdbf017572b182443ce159ed3e5fdff9f0fa
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-12 16:47:53 -0800

    Added auth check

--- app/Http/Requests/Auth/ResetPasswordRequest.php
+++ app/Http/Requests/Auth/ResetPasswordRequest.php
@@ -11,7 +11,7 @@ final class ResetPasswordRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true;
+        return auth()->check();
     }
 
     public function rules(): array
