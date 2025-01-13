fb423d497cca65731c3e939271fbcbada1770e69
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-12 16:55:31 -0800

    Renamed request; code cleanup

--- app/Livewire/Contact/ContactMessageForm.php
+++ app/Livewire/Contact/ContactMessageForm.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace App\Livewire\Contact;
 
-use App\Http\Requests\Contact\SendContactMessageRequest;
+use App\Http\Requests\Contact\StoreContactMessageRequest;
 use Livewire\Form;
 
 final class ContactMessageForm extends Form
 {
     protected function rules(): array
     {
-        return (new SendContactMessageRequest())->rules();
+        return new StoreContactMessageRequest()->rules();
     }
 }
