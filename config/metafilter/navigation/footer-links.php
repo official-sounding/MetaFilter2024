90a7bf5303fc5e71f34c33e274f84c22bca8b9ed
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-14 11:56:23 -0800

    Removed outdated links

--- config/metafilter/navigation/footer-links.php
+++ config/metafilter/navigation/footer-links.php
@@ -6,8 +6,8 @@ use App\Enums\RouteNameEnum;
 
 return [
     [
-        'name' => 'Home',
-        'route' => RouteNameEnum::MetaFilterPostIndex,
+        'name' => 'FAQ',
+        'route' => RouteNameEnum::FaqIndex,
     ],
     [
         'name' => 'Guidelines',
@@ -22,30 +22,6 @@ return [
         'route' => RouteNameEnum::MetaFilterPostIndex,
     ],
     [
-        'name' => 'FAQ',
-        'route' => RouteNameEnum::FaqIndex,
-    ],
-    [
-        'name' => 'About',
-        'route' => RouteNameEnum::AboutMetaFilter,
-    ],
-    [
-        'name' => 'Archives',
-        'route' => RouteNameEnum::MetaFilterPostIndex,
-    ],
-    [
-        'name' => 'Tags',
-        'route' => RouteNameEnum::TagsIndex,
-    ],
-    [
-        'name' => 'Popular',
-        'route' => RouteNameEnum::MetaFilterPostIndex,
-    ],
-    [
-        'name' => 'Random',
-        'route' => RouteNameEnum::MetaFilterPostIndex,
-    ],
-    [
         'name' => 'BIPOC Board',
         'route' => RouteNameEnum::MetaFilterPostIndex,
     ],
@@ -56,17 +32,5 @@ return [
     [
         'name' => 'Wiki',
         'route' => RouteNameEnum::MetaFilterPostIndex,
-    ],
-    [
-        'name' => 'Search',
-        'route' => RouteNameEnum::SearchCreate,
-    ],
-    [
-        'name' => 'Chat',
-        'route' => RouteNameEnum::ChatHomeIndex,
-    ],
-    [
-        'name' => 'Labs',
-        'route' => RouteNameEnum::LabsHomeIndex,
     ],
 ];
