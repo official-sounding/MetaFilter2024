4f68f563543a452bca7eea4ac7545b0ddb5a222d
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-14 11:55:14 -0800

    Route updates

--- routes/auth.php
+++ routes/auth.php
@@ -1,84 +1,45 @@
 <?php
 
-/*
-
 declare(strict_types=1);
 
 use App\Enums\RouteNameEnum;
-use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SignupController;
 use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
-use Laravel\Fortify\Http\Controllers\ConfirmablePasswordController;
 use Laravel\Fortify\Http\Controllers\NewPasswordController;
 use Laravel\Fortify\Http\Controllers\PasswordResetLinkController;
 use Laravel\Fortify\Http\Controllers\RegisteredUserController;
+use Laravel\Fortify\RoutePath;
 
-$domain = 'www.' . config('app.url');
+Route::post(RoutePath::for('login', '/login'), [AuthenticatedSessionController::class, 'store'])
+    ->middleware(array_filter([
+        'guest:' . config('fortify.guard'),
+    ]))->name(RouteNameEnum::AuthLoginStore);
 
-Route::domain($domain)
-    ->middleware('guest')->group(
-        function () {
-            Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-                ->name(RouteNameEnum::AuthForgotPasswordCreate);
+Route::post(RoutePath::for('logout', '/logout'), [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware([config('fortify.auth_middleware', 'auth') . ':' . config('fortify.guard')])
+    ->name('logout');
 
-            Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->name(RouteNameEnum::AuthForgotPasswordStore);
+Route::get(RoutePath::for('password.request', '/forgot-password'), [PasswordResetLinkController::class, 'create'])
+    ->middleware(['guest:' . config('fortify.guard')])
+    ->name('password.request');
 
-            Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name(RouteNameEnum::AuthLoginCreate);
+Route::post(RoutePath::for('password.email', '/forgot-password'), [PasswordResetLinkController::class, 'store'])
+    ->middleware(['guest:' . config('fortify.guard')])
+    ->name('password.email');
 
-            Route::post('login', [AuthenticatedSessionController::class, 'store'])
-                ->name(RouteNameEnum::AuthLoginStore)
-                ->middleware([HandlePrecognitiveRequests::class]);
+Route::get(RoutePath::for('password.reset', '/reset-password/{token}'), [NewPasswordController::class, 'create'])
+    ->middleware(['guest:' . config('fortify.guard')])
+    ->name('password.reset');
 
-            Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name(RouteNameEnum::AuthRegisterCreate);
+Route::post(RoutePath::for('password.update', '/reset-password'), [NewPasswordController::class, 'store'])
+    ->middleware(['guest:' . config('fortify.guard')])
+    ->name('password.update');
 
-            Route::post('register', [RegisteredUserController::class, 'store'])
-                ->name(RouteNameEnum::AuthRegisterStore);
+Route::get('signup', [RegisteredUserController::class, 'create'])
+    ->name(RouteNameEnum::SignupCreate);
 
-            Route::post('reset-password', [NewPasswordController::class, 'store'])
-                ->name(RouteNameEnum::AuthResetPasswordStore);
+Route::get('signup/thanks', [SignupController::class, 'thanks'])
+    ->name(RouteNameEnum::SignupThanks);
 
-            Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-                ->name(RouteNameEnum::AuthResetPasswordCreate);
-        },
-    );
-
-Route::domain($domain)
-    ->middleware('auth')->group(
-        function () {
-            Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->name(RouteNameEnum::AuthLogout);
-
-            Route::get('user/confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->name(RouteNameEnum::AuthConfirmPasswordShow);
-
-            Route::put('user/password', [ConfirmablePasswordController::class, 'show'])
-                ->name(RouteNameEnum::AuthConfirmPasswordShow);
-
-            Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
-        },
-    );
-
-
-Route::domain('www.' . $appUrl)
-    ->middleware('auth')->group(
-        function () {
-            Route::get('verify-email', EmailVerificationPromptController::class)
-                ->name('verification.notice');
-
-            Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-                ->middleware(['signed', 'throttle:6,1'])
-                ->name('verification.verify');
-
-            Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware('throttle:6,1')
-                ->name('verification.send');
-
-
-            Route::put('password', [PasswordController::class, 'update'])
-                ->name('password.update');
-        },
-    );
-*/
+Route::get('signup/wizard', [SignupController::class, 'wizard'])
+    ->name(RouteNameEnum::SignupWizard);
