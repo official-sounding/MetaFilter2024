efabd2f4cafc08a759eca762181c846ce5bc62ae
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-14 11:56:37 -0800

    Disabled Features::registration

--- config/fortify.php
+++ config/fortify.php
@@ -146,7 +146,7 @@ return [
     */
 
     'features' => [
-        Features::registration(),
+        // Features::registration(),
         Features::resetPasswords(),
         Features::emailVerification(),
         //        Features::updateProfileInformation(),
@@ -159,5 +159,4 @@ return [
         ]),
   */
     ],
-
 ];
