43a2e866c23041003a2f4db636f31b0fd5e3b578
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-14 11:56:50 -0800

    Removed LivewireServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,7 +6,6 @@ use App\Providers\AppServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
 use App\Providers\FortifyServiceProvider;
 use App\Providers\HorizonServiceProvider;
-use App\Providers\LivewireServiceProvider;
 use App\Providers\RepositoryServiceProvider;
 use App\Providers\TelescopeServiceProvider;
 use App\Providers\ViewComposerServiceProvider;
@@ -16,7 +15,6 @@ return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
     HorizonServiceProvider::class,
-    LivewireServiceProvider::class,
     RepositoryServiceProvider::class,
     TelescopeServiceProvider::class,
     ViewComposerServiceProvider::class,
