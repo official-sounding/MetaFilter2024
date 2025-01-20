ef419daa41ab065bce39935879de0ef4eac81f9f
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-20 12:13:33 -0800

    Removed cache

--- app/View/Composers/BannerLinks/BannerLinkViewComposer.php
+++ app/View/Composers/BannerLinks/BannerLinkViewComposer.php
@@ -7,15 +7,12 @@ namespace App\View\Composers\BannerLinks;
 use App\Models\BannerLink;
 use App\View\Composers\ViewComposerInterface;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Cache;
 
 final class BannerLinkViewComposer implements ViewComposerInterface
 {
     public function compose(View $view): void
     {
-        $bannerLinks = Cache::rememberForever('banner-links', function () {
-            return BannerLink::all()->collect();
-        });
+        $bannerLinks = BannerLink::all()->collect();
 
         $view->with([
             'bannerLinks' => $bannerLinks,
