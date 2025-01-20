21dae3ce6fd9cf0bd9bfbf807061ed1900677998
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-20 12:12:59 -0800

    Added model and fields

--- database/factories/BannerLinkFactory.php
+++ database/factories/BannerLinkFactory.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 
 namespace Database\Factories;
 
+use App\Models\BannerLink;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 final class BannerLinkFactory extends Factory
 {
-    protected $model = App\Models\BannerLink;
+    protected $model = BannerLink::class;
 
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->words(3, true),
+            'url' => $this->faker->url(),
+            'created_at' => $this->faker->dateTimeThisYear(),
         ];
     }
 }
