f684e73b53b7de4323a69b1cd117093ca5b9c99a
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-20 21:08:33 -0800

    Fixed typos

--- resources/views/signup/thanks.blade.php
+++ resources/views/signup/thanks.blade.php
@@ -5,7 +5,7 @@
 @section('contents')
     <p>
         You should receive an email shortly with a link to verify your email address.
-        If you don&nbsp;t see it, check your spam folder.
-        If you still don&nbsp;t see it, <a href="/contact">let us know</a> and we&nbsp;ll help you out.
+        If you don&rsquo;t see it, check your spam folder.
+        If you still don&rsquo;t see it, <a href="/contact">let us know</a> and we&rsquo;ll help you out.
     </p>
 @endsection
