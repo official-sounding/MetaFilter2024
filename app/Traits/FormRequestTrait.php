85cf62ff73d19e97ab66390176dbf7faecb3582a
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-21 19:43:49 -0800

    Moved functions to trait

---
+++ app/Traits/FormRequestTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+
+trait FormRequestTrait {
+    public function loggedIn(): bool
+    {
+        return auth()->check();
+    }
+
+    public function loggedOut(): bool
+    {
+        return !auth()->check();
+    }
+}
