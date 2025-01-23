dadeb6a731c07af7f913762e2d4a66fe3d5fc95d
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-22 16:52:02 -0800

    Installed vormkracht10/laravel-mails

---
+++ database/migrations/2025_01_22_223316_2_create_mailables_table.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create(config('mails.database.tables.polymorph'), function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(config('mails.models.mail'))
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->morphs('mailable');
+        });
+    }
+};
