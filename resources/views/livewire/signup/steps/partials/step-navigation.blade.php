414a171f0da40ba77967709a9bffb6a2c65afe40
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-22 21:48:38 -0800

    Deleted old views

--- resources/views/livewire/signup/steps/partials/step-navigation.blade.php
+++
@@ -1,9 +0,0 @@
-<ol class="steps">
-    @foreach($steps as $step)
-        <li class="{{ $step->isCurrent() ? 'text-bold' : '' }}"
-            @if ($step->isPrevious())
-                wire:click="{{ $step->show() }}"
-            @endif
-        >{{ $step->label }}</li>
-    @endforeach
-</ol>
