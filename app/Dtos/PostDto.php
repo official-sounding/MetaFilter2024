81321c82205f0d8d740509035e0625ec2d9b01a9
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-25 11:37:26 -0800

    Added DTOs

---
+++ app/Dtos/PostDto.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Dtos;
+
+readonly class PostDto
+{
+    public function __construct(
+        public string $title,
+        public string $body,
+        public string $more_inside,
+        public int $subsite_id,
+        public string $status,
+        public string $published_at,
+        public bool $is_published,
+    ) {}
+}
---
+++ app/Dtos/UserDto.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Dtos;
+
+readonly class UserDto
+{
+    public function __construct(
+        public string $username,
+        public string $password,
+        public string $email,
+        public ?string $name,
+        public ?bool $homepage_url,
+    ) {}
+}
