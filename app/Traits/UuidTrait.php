bdceee50228208abeb39f98976785d64d3593c54
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-24 19:52:34 -0800

    Added trait

---
+++ app/Traits/UuidTrait.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+use Ramsey\Uuid\Uuid;
+
+trait UuidTrait {
+    public function getUuid(): string
+    {
+        return (string) Uuid::uuid4();
+    }
+}
