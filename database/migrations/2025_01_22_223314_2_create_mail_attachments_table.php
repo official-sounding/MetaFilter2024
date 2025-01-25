debfeebfdea6f1414c6245da672e68715dfd8ff1
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-25 11:40:27 -0800

    Installed vormkracht10/filament-mails

--- database/migrations/2025_01_22_223314_2_create_mail_attachments_table.php
+++ database/migrations/2025_01_22_223314_2_create_mail_attachments_table.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    public function up()
+    public function up(): void
     {
         Schema::create(config('mails.database.tables.attachments', 'mail_attachments'), function (Blueprint $table) {
             $table->id();
--- database/migrations/2025_01_22_223315_2_create_mail_events_table.php
+++ database/migrations/2025_01_22_223315_2_create_mail_events_table.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    public function up()
+    public function up(): void
     {
         Schema::create(config('mails.database.tables.events', 'mail_events'), function (Blueprint $table) {
             $table->id();
