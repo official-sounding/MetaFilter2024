b8b925d0f9863660338632aa3119f989c5b8d5c7
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-25 09:48:01 -0800

    Added icon placeholder

--- config/metafilter/navigation/utility.php
+++ config/metafilter/navigation/utility.php
@@ -24,6 +24,7 @@ return [
         [
             'name' => 'Favorites',
             'route' => RouteNameEnum::FavoritesIndex,
+            'icon' => '',
         ],
         [
             'name' => 'Recent Activity',
