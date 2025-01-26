435f30442ec074975ad70269188e1be21d346494
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-25 17:18:37 -0800

    Factory updates

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -12,6 +12,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 final class CommentFactory extends Factory
 {
     protected $model = Comment::class;
+    protected const string POST_MODEL = 'App\Models\Post';
+    protected const string USER_MODEL = 'App\Models\User';
 
     public function definition(): array
     {
@@ -19,12 +21,25 @@ final class CommentFactory extends Factory
         $timestamp = date('Y-m-d H:i:s', strtotime($timestamp));
 
         return [
-            'body' => $this->faker->paragraph(),
-            'post_id' => Post::inRandomOrder()->first(),
-            'user_id' => User::inRandomOrder()->first(),
+            'text' => $this->faker->paragraph(),
+            'parent_id' => null,
+            'reply_id' => null,
+            'commentable_type' => self::POST_MODEL,
+            'commentable_id' => (new Post)->inRandomOrder()->first(),
+            'commenter_type' => self::USER_MODEL,
+            'commenter_id' => (new User)->inRandomOrder()->first(),
             'created_at' => $timestamp,
             'updated_at' => null,
             'deleted_at' => null,
         ];
+    }
+
+    public function hasReply(): Factory
+    {
+        return $this->state(function () {
+            return [
+                'reply_id' => (new Comment)->inRandomOrder()->first(),
+            ];
+        });
     }
 }
