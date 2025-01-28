12a3598b5d3af2e1da363e8670fd37c74c19f2aa
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-27 22:08:47 -0800

    Removed unused MorphMany

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,6 @@ use Filament\Panel;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
