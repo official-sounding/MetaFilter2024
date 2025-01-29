8bed56794c94c18ac401c81670bb547a039947f3
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-28 22:44:50 -0800

    Added declare(strict_types=1); code cleanup

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,23 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
-class AuthServiceProvider extends ServiceProvider
+final class AuthServiceProvider extends ServiceProvider
 {
-    /**
-    * The model to policy mappings for the application.
-    *
-    * @var array<class-string, class-string>
-    */
     protected $policies = [
         // Model::class => ModelPolicy::class,
     ];
 
-    /**
-    * Register any authentication / authorization services.
-    */
     public function boot(): void
     {
         $this->registerPolicies();
