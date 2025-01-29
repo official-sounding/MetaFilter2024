942e28ab06b90bcadcd62a45314e7b4f315cd026
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-29 07:19:23 -0800

    Deleted unused views

--- resources/views/livewire/comments/chunks-component.blade.php
+++
@@ -1,8 +0,0 @@
-@for ($chunk = 0; $chunk < $page; $chunk++)
-    <div wire:key="chunk-{{ $chunk }}">
-            <livewire:comment-chunk
-                :ids="$chunks[$chunk]"
-                wire:key="chunk-{{ md5(json_encode($this->chunks[$chunk])) }}"
-            />
-    </div>
-@endfor
