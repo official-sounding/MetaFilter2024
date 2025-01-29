70ff59a34c83c714ca73b422862bb326b9918645
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-28 23:17:12 -0800

    Replaced array with DTO

--- app/Dtos/SubsiteDto.php
+++ app/Dtos/SubsiteDto.php
@@ -5,5 +5,20 @@ declare(strict_types=1);
 namespace App\Dtos;
 
 final class SubsiteDto {
-
+    public function __construct(
+        public string $name,
+        public string $nickname,
+        public string $logo_filename,
+        public string $white_text,
+        public string $green_text,
+        public string $tagline,
+        public string $subdomain,
+        public string $route,
+        public string $view,
+        public bool $has_theme,
+        public bool $in_dropdown,
+        public bool $in_footer_nav,
+        public int $footer_navigation_sort_order,
+        public int $global_navigation_sort_order,
+    ) {}
 }
--- database/seeders/Production/SubsiteSeeder.php
+++ database/seeders/Production/SubsiteSeeder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Database\Seeders\Production;
 
+use App\Dtos\SubsiteDto;
 use App\Models\Subsite;
 use App\Traits\StringFormattingTrait;
 use Illuminate\Database\Seeder;
@@ -14,35 +15,27 @@ final class SubsiteSeeder extends Seeder
 
     public function run(): void
     {
-        $subsites = [];
+        $subsites = config('metafilter.seeders.subsites');
 
-        $subsiteData = config('metafilter.seeders.subsites');
+        foreach ($subsites as $subsite) {
+            $dto = new SubsiteDto(
+                name: $subsite['name'],
+                nickname: $subsite['nickname'] ?? null,
+                logo_filename: $subsite['logo_filename'] ?? null,
+                white_text: $subsite['white_text'],
+                green_text: $subsite['green_text'],
+                tagline: $subsite['tagline'] ?? null,
+                subdomain: $subsite['subdomain'],
+                route: $subsite['route'],
+                view: $subsite['view'],
+                has_theme: $subsite['hasTheme'] ?? false,
+                in_dropdown: $subsite['inDropdown'] ?? false,
+                in_footer_nav: $subsite['inFooterNav'] ?? false,
+                footer_navigation_sort_order: $subsite['footerNavigationSortOrder'] ?? 0,
+                global_navigation_sort_order: $subsite['globalNavigationSortOrder'] ?? 0
+            );
 
-        $now = now();
-
-        foreach ($subsiteData as $subsite) {
-            // TODO: Move to DTO?
-            $subsites[] = [
-                'name' => $subsite['name'],
-                'slug' => $this->getSlug($subsite['name']),
-                'nickname' => $subsite['nickname'] ?? null,
-                'logo_filename' => $subsite['logoFilename'] ?? null,
-                'white_text' => $subsite['whiteText'] ?? null,
-                'green_text' => $subsite['greenText'] ?? null,
-                'tagline' => $subsite['tagline'] ?? null,
-                'subdomain' => $subsite['subdomain'],
-                'route' => $subsite['route'],
-                'view' => $subsite['view'],
-                'has_theme' => $subsite['hasTheme'] ?? false,
-                'in_dropdown' => $subsite['inDropdown'] ?? false,
-                'in_footer_nav' => $subsite['inFooterNav'] ?? false,
-                'footer_navigation_sort_order' => $subsite['footerNavigationSortOrder'] ?? 0,
-                'global_navigation_sort_order' => $subsite['globalNavigationSortOrder'] ?? 0,
-                'created_at' => $now,
-                'updated_at' => null,
-            ];
+            Subsite::firstOrCreate((array) $dto);
         }
-
-        Subsite::upsert($subsites, 'name');
     }
 }
