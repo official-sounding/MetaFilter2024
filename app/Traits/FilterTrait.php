f230aed2071d9b116f2f7730f0f493ded7cae039
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-01-31 11:05:42 -0800

    Added unacceptable words filter

---
+++ app/Traits/FilterTrait.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+
+trait FilterTrait {
+    public function hasUnacceptableWords(string $text): bool
+    {
+        $unacceptableWords = config('config.unacceptable_words');
+
+        return stripos(json_encode($unacceptableWords), $text);
+    }
+}
