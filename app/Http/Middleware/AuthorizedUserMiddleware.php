08855b7d2d4931b9ed9da30d00ae5d26424875f0
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-03 14:01:53 -0800

    Added AuthorizedUserMiddleware

---
+++ app/Http/Middleware/AuthorizedUserMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class AuthorizedUserMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->user->id == auth()->id()) {
+            return $next($request);
+        }
+
+        abort(Response::HTTP_FORBIDDEN);
+    }
+}
