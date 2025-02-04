57ef905b3a7137fa9a4c888bcf65d2547bbdc810
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-04 13:04:12 -0800

    Made title dynamic

--- app/Http/Controllers/MyMeFiController.php
+++ app/Http/Controllers/MyMeFiController.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Traits\MyMeFiTrait;
 use Illuminate\Contracts\View\View;
 
 final class MyMeFiController extends BaseController
 {
+    use MyMeFiTrait;
+
     public function index(): View
     {
         return view('my-mefi.index', [
-            'title' => 'My MeFi',
+            'title' => $this->getTitle(),
         ]);
     }
 }
