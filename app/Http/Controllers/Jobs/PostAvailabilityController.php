41261abe4b66409d1def16d99eb79f65721a0eb6
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-05 01:01:20 -0800

    Added controllers

--- app/Http/Controllers/Jobs/PostAvailabilityController.php
+++ app/Http/Controllers/Jobs/PostAvailabilityController.php
@@ -1,7 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Jobs;
 
-class PostAvailabilityController {
+use App\Http\Controllers\BaseController;
+use Illuminate\Contracts\View\View;
 
+final class PostAvailabilityController extends BaseController
+{
+    public function create(): View
+    {
+        return view('jobs.post-availability.create', [
+            'title' => trans('Post Your Availability'),
+        ]);
+    }
 }
--- app/Http/Controllers/Jobs/PostJobController.php
+++ app/Http/Controllers/Jobs/PostJobController.php
@@ -1,7 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Jobs;
 
-class PostJobController {
+use App\Http\Controllers\BaseController;
+use Illuminate\Contracts\View\View;
 
+final class PostJobController extends BaseController
+{
+    public function create(): View
+    {
+        return view('jobs.post-job.create', [
+            'title' => trans('Post a Job'),
+        ]);
+    }
 }
