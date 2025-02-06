dfde369bc171c971eb72db44e2afef2ef609ce8e
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-06 08:26:39 -0800

    Code cleanup

--- resources/views/livewire/comments/comment-form-component.blade.php
+++ resources/views/livewire/comments/comment-form-component.blade.php
@@ -2,27 +2,29 @@
     @include('forms.partials.validation-summary')
     @include('livewire.posts.partials.posting-as')
 
-    <div wire:ignore>
-        <label for="text" class="sr-only">
-            {{ trans('Comment') }}
-        </label>
+    <fieldset>
+        <div wire:ignore>
+            <label for="text" class="sr-only">
+                {{ trans('Comment') }}
+            </label>
 
-        <textarea wire:model="text" name="text" id="text"></textarea>
-    </div>
+            <textarea wire:model="text" name="text" id="text"></textarea>
+        </div>
 
-    <div class="level">
-        <button type="button" class="button secondary-button" wire:click="{{ $cancelAction }}">
-            {{ trans('Cancel') }}
-        </button>
+        <div class="level">
+            <button type="button" class="button secondary-button" wire:click="{{ $cancelAction }}">
+                {{ trans('Cancel') }}
+            </button>
 
-        <button type="submit" class="button primary-button" wire:keydown.escape="{{ $cancelAction }}">
-            @if (!empty($buttonText))
-                {{ trans($buttonText) }}
-            @else
-                {{ trans('Add Comment') }}
-            @endif
-        </button>
-    </div>
+            <button type="submit" class="button primary-button" wire:keydown.escape="{{ $cancelAction }}">
+                @if (!empty($buttonText))
+                    {{ trans($buttonText) }}
+                @else
+                    {{ trans('Add Comment') }}
+                @endif
+            </button>
+        </div>
+    </fieldset>
 </form>
 {{--
 @push('scripts')
