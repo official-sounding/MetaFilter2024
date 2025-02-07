24d672aae83ff6776768e84ead726407270262ec
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-06 19:59:37 -0800

    Changed request

--- app/Livewire/Posts/PostForm.php
+++ app/Livewire/Posts/PostForm.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace App\Livewire\Posts;
 
-use App\Http\Requests\Post\StoreMetaFilterPostRequest;
+use App\Http\Requests\Post\StorePostRequest;
 use Livewire\Form;
 
 final class PostForm extends Form
 {
     protected function rules(): array
     {
-        return (new StoreMetaFilterPostRequest())->rules();
+        return (new StorePostRequest())->rules();
     }
 }
