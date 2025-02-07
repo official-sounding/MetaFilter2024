12a1b2a8d4c17a2abc41230c60aa93e746e4a213
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-07 15:35:33 -0800

    Added board member

--- app/Enums/RoleNameEnum.php
+++ app/Enums/RoleNameEnum.php
@@ -6,12 +6,14 @@ namespace App\Enums;
 
 enum RoleNameEnum: string
 {
+    case BOARD_MEMBER = 'board member';
     case DEVELOPER = 'developer';
     case MODERATOR = 'moderator';
 
     public function label(): string
     {
         return match ($this) {
+            RoleNameEnum::BOARD_MEMBER => 'Board Member',
             RoleNameEnum::DEVELOPER => 'Developers',
             RoleNameEnum::MODERATOR => 'Moderators',
         };
