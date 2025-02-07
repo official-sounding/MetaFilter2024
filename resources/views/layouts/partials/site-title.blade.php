136448d42e810fbf748b32361b60d8817647b494
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-07 10:22:56 -0800

    Added check for subsite

--- resources/views/layouts/partials/site-title.blade.php
+++ resources/views/layouts/partials/site-title.blade.php
@@ -1,17 +1,19 @@
-<a href="{{ route($subsite['route']) }}" title="Home page">
-    <hgroup class="site-title">
-        @if (empty(request()->segment(1)))
-            <h1 class="site-title-text">
-                @include('layouts.partials.site-title-text')
-            </h1>
-        @else
-            <p class="site-title-text">
-                @include('layouts.partials.site-title-text')
-            </p>
-        @endif
+@if (!empty($subsite))
+    <a href="{{ route($subsite['route']) }}" title="Home page">
+        <hgroup class="site-title">
+            @if (empty(request()->segment(1)))
+                <h1 class="site-title-text">
+                    @include('layouts.partials.site-title-text')
+                </h1>
+            @else
+                <p class="site-title-text">
+                    @include('layouts.partials.site-title-text')
+                </p>
+            @endif
 
-        @if (isset($tagline))
-            <p class="tagline">{{ $tagline }}</p>
-        @endif
-    </hgroup>
-</a>
+            @if (isset($tagline))
+                <p class="tagline">{{ $tagline }}</p>
+            @endif
+        </hgroup>
+    </a>
+@endif
