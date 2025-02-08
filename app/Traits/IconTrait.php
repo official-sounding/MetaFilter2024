5cde18397dc8d8b27385ecc88b50c6d8c75de5a7
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-07 23:53:29 -0800

    Added IconTrait

---
+++ app/Traits/IconTrait.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+trait IconTrait {
+    public function getIconPath(string $iconFilename): string
+    {
+        return "images/icons/$iconFilename.svg";
+    }
+}
