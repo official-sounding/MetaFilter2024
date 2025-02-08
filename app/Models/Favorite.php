d10d2b382aab3214c6a6fefc536c45e8fc31144a
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-08 13:03:41 -0800

    Added favoritable

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -7,12 +7,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property int $favoritable_id
+ * @property string $favoritable_type
+ */
 final class Favorite extends BaseModel
 {
     // Properties
 
     protected $fillable = [
         'user_id',
+        'favoritable_id',
+        'favoritable_type',
     ];
 
     public function favoritable(): MorphTo
