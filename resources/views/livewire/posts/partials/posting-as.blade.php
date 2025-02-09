647eb41f300a30b8b535b937fc2fa280c42623bf
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-09 12:38:09 -0800

    Added space and translation

--- resources/views/livewire/posts/partials/posting-as.blade.php
+++ resources/views/livewire/posts/partials/posting-as.blade.php
@@ -1,4 +1,4 @@
-<small>posting as
+<small class="space">{{ trans('posting as') }}
     <a title="{{ trans('View profile') }}"
        href="{{ route($memberShowRoute, [
             'user' => auth()->user()
