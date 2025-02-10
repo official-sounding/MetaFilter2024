d5fbd1fcb5b0d9b3f2bc98fd449439a494af435c
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-10 08:34:50 -0800

    Positioned to-top/bottom buttons

--- resources/views/layouts/navigation/secondary-navigation.blade.php
+++ resources/views/layouts/navigation/secondary-navigation.blade.php
@@ -1,7 +1,5 @@
 @if ($secondaryNavigation)
     <nav class="secondary-navigation container" id="secondary-navigation">
         {!! $secondaryNavigation !!}
-
-        <x-buttons.top-bottom-button-component location='top' />
     </nav>
 @endif
