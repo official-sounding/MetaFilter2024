e90c0c4dfa85f6342342e91a369d7e6efb57fa04
Kirk Franklin <kirkfranklin@alamedaca.gov>
2025-02-13 15:43:18 -0800

    Added DTO

---
+++ app/Dtos/FavoriteDto.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Dtos;
+
+readonly class FavoriteDto
+{
+    public function __construct(
+        public string $favoritableType,
+        public int $favoritableId,
+        public int $userId
+    ) {}
+}
