a14624a13aff8addcd158682d5b5a079a624a676
Kirk Franklin <kirkfranklin@alamedaca.gov>
2025-02-12 16:04:57 -0800

    Get icon path from trait

--- app/View/Components/Icons/IconComponent.php
+++ app/View/Components/Icons/IconComponent.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace App\View\Components\Icons;
 
+use App\Traits\IconTrait;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 final class IconComponent extends Component
 {
+    use IconTrait;
+
     public string $filename;
     public ?string $altText;
     public ?string $titleText;
@@ -23,14 +26,9 @@ final class IconComponent extends Component
     public function render(): View
     {
         return view('components.icons.icon', [
-            'filePath' => $this->getFilePath(),
+            'filePath' => $this->getIconPath($this->filename),
             'altText' => $this->altText,
             'titleText' => $this->titleText,
         ]);
-    }
-
-    private function getFilePath(): string
-    {
-        return "images/icons/{{ $this->filename }}.svg";
     }
 }
