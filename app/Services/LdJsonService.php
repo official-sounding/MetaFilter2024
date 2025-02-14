13ab2ef6aa4fb09d7d3cd6e163fbf8228929abbd
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-14 10:42:44 -0800

    Added logging; date formatting

--- app/Services/LdJsonService.php
+++ app/Services/LdJsonService.php
@@ -5,17 +5,33 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Models\Post;
+use App\Traits\LoggingTrait;
+use DateMalformedStringException;
+use DateTime;
 use Spatie\SchemaOrg\Schema;
 
 final class LdJsonService
 {
+    use LoggingTrait;
+
     public function getBlogPosting(Post $post): string
     {
+        try {
+            $dateModified = new DateTime($post->updated_at);
+            $datePublished = new DateTime($post->created_at);
+        } catch (DateMalformedStringException $exception) {
+            $this->logError($exception);
+
+            $dateModified = new DateTime();
+            $datePublished = new DateTime();
+        }
+
         $blogPosting = Schema::blogPosting()
             ->headline($post->title)
-            ->datePublished($post->created_at)
-            ->dateModified($post->updated_at)
+            ->datePublished($datePublished)
+            ->dateModified($dateModified)
             ->author($post->user->username)
+            // TODO: Get canonical URL
             ->mainEntityOfPage($post->url)
             ->articleBody($post->body . $post->more_inside);
 
