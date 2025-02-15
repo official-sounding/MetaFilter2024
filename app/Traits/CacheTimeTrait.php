cb35f06be4466c20b5caafa938e2a55dd5fa6991
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-15 14:16:41 -0800

    Added caching durations

--- app/Traits/CacheTimeTrait.php
+++ app/Traits/CacheTimeTrait.php
@@ -4,10 +4,32 @@ declare(strict_types=1);
 
 namespace App\Traits;
 
+use Illuminate\Support\Carbon;
+
 trait CacheTimeTrait
 {
-    public function oneHour(): int
+    public function oneHour(): Carbon
     {
-        return 60;
+        return now()->addHour();
+    }
+
+    public function oneDay(): Carbon
+    {
+        return now()->addDay();
+    }
+
+    public function days(int $days): Carbon
+    {
+        return now()->addDays($days);
+    }
+
+    public function oneWeek(): Carbon
+    {
+        return now()->addWeek();
+    }
+
+    public function oneMonth(): Carbon
+    {
+        return now()->addMonth();
     }
 }
