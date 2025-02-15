a681c15ab886cb891a5758d6813223e0124b713e
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-14 18:31:03 -0800

    Added StoreLoginRequest

---
+++ app/Http/Requests/Auth/StoreLoginRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Auth;
+
+use App\Http\Requests\BaseFormRequest;
+use App\Traits\AuthStatusTrait;
+
+final class StoreLoginRequest extends BaseFormRequest
+{
+    use AuthStatusTrait;
+
+    public function authorize(): bool
+    {
+        return $this->loggedOut();
+    }
+
+    public function rules(): array
+    {
+        return [
+            'username' => [
+                'required',
+                'string',
+            ],
+            'password' => [
+                'required',
+                'string',
+            ],
+        ];
+    }
+}
