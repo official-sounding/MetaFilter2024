430a7e1d4e2f8a9f0ec7530a53929501b52ed2b6
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-14 16:40:11 -0800

    Code cleanup

--- resources/views/members/partials/update-password-form.blade.php
+++ resources/views/members/partials/update-password-form.blade.php
@@ -9,36 +9,58 @@
         </p>
     </header>
 
-    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('password.update') }}">
         @include('forms.partials.csrf-token')
         @method('put')
 
         <div>
-            <label for="update_password_current_password">{{ trans('Current Password') }}</label>
-            <input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
+            <label for="update_password_current_password">
+                {{ trans('Current Password') }}
+            </label>
+            <input
+                id="update_password_current_password"
+                name="current_password"
+                type="password"
+                autocomplete="current-password"
+            />
         </div>
 
         <div>
-            <label for="update_password_password">{{ trans('New Password') }}</label>
-            <input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+            <label for="update_password_password">
+                {{ trans('New Password') }}
+            </label>
+            <input
+                id="update_password_password"
+                name="password"
+                type="password"
+                autocomplete="new-password"
+            />
         </div>
 
         <div>
-            <label for="update_password_password_confirmation">{{ trans('Confirm Password') }}</label>
-            <input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+            <label for="update_password_password_confirmation">
+                {{ trans('Confirm Password') }}
+            </label>
+            <input
+                id="update_password_password_confirmation"
+                name="password_confirmation"
+                type="password"
+                autocomplete="new-password"
+            />
         </div>
 
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ trans('Save') }}</x-primary-button>
+        <div>
+            <button class="button primary-button">
+                {{ trans('Save') }}
+            </button>
 
             @if (session('status') === 'password-updated')
-                <p
-                    x-data="{ show: true }"
+                <p x-data="{ show: true }"
                     x-show="show"
                     x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
-                >{{ trans('Saved.') }}</p>
+                    x-init="setTimeout(() => show = false, 2000)">
+                    {{ trans('Saved.') }}
+                </p>
             @endif
         </div>
     </form>
