f583c34c9eed9f7498f67e00b41767d7ac092a5c
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-14 23:07:03 -0800

    Copy change

--- resources/views/livewire/wizards/signup/steps/01-username-step.blade.php
+++ resources/views/livewire/wizards/signup/steps/01-username-step.blade.php
@@ -6,7 +6,7 @@
         <x-forms.input
             :name="'username'"
             :type="'text'"
-            :note="'Your username will be displayed on the site with your posts and comments and can&rsquo;t be changed.
+            :note="'Your username will be displayed with your posts and comments and can&rsquo;t be changed.
                     Usernames may consist of English letters, numbers, and most punctuation.'"
             :label="'First, pick your username'"
             :autofocus="false"
