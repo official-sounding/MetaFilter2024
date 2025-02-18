b2c2330d782e1927faddbec76431d8b66d185522
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-18 09:18:14 -0800

    Code cleanup

--- app/Repositories/UserRepositoryInterface.php
+++ app/Repositories/UserRepositoryInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
-use App\Models\User;
-
 interface UserRepositoryInterface extends BaseRepositoryInterface
 {
     public function getActiveMembers();
