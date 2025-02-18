e83c54488ef1609f136c1f914808ddd2b325fb75
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-18 08:43:16 -0800

    Added fields

--- app/Dtos/PostDto.php
+++ app/Dtos/PostDto.php
@@ -4,15 +4,18 @@ declare(strict_types=1);
 
 namespace App\Dtos;
 
-readonly class PostDto
+final class PostDto
 {
     public function __construct(
         public string $title,
+        public ?string $link_url,
+        public ?string $link_text,
         public string $body,
         public string $more_inside,
+        public int $user_id,
         public int $subsite_id,
         public string $state,
-        public string $published_at,
+        public ?string $published_at,
         public bool $is_published,
     ) {}
 }
