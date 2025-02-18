a27ce7f863003bcd3e31bd015828a39d90584fdc
Kirk Franklin <kirkfranklin@alamedaca.gov>
2025-02-18 10:46:42 -0800

    Optimized component

--- resources/views/livewire/site-banner/site-banner-component.blade.php
+++ resources/views/livewire/site-banner/site-banner-component.blade.php
@@ -1,4 +1,6 @@
-<section class="site-banner collapsible-container @if ($isExpanded === true) is-expanded @endif">
+<section
+    class="site-banner collapsible-container @if ($isExpanded === true) is-expanded @endif"
+    id="site-banner">
     @if ($bannerLinks)
         <ul class="site-banner-links collapsible-contents">
             @foreach ($bannerLinks as $link)
@@ -10,8 +12,17 @@
             @endforeach
         </ul>
 
-        <button class="button icon" wire:click="toggleBanner()">
-            <img src="{{ asset("images/icons/$iconFilename.svg") }}" alt="{{ $altText }} {{ trans('banner links') }}">
+        <button
+            wire:click="toggleBanner()"
+            aria-expanded="{{ $isExpanded }}"
+            aria-controls="site-banner"
+            aria-label="Help about username"
+            class="button icon">
+            <img src="{{ asset("images/icons/$iconFilename.svg") }}" alt="{{ trans($altText) }}">
         </button>
+    @else
+        @include('notifications.none-listed', [
+            'records' => 'banner links'
+        ])
     @endif
 </section>
