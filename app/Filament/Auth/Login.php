2284bd13bc0a10792b837120b951283799d492bb
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-18 17:07:00 -0800

    Added custom Filament login

---
+++ app/Filament/Auth/Login.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Auth;
+
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
+use DanHarrin\LivewireRateLimiting\WithRateLimiting;
+use Filament\Facades\Filament;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Http\Responses\Auth\Contracts\LoginResponse;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+
+final class Login extends Component implements HasForms
+{
+    use InteractsWithForms;
+    use WithRateLimiting;
+
+    public string $email = '';
+    public string $password = '';
+    public bool $remember = false;
+    private mixed $form;
+
+    public function mount(): void
+    {
+        if (Filament::auth()->check()) {
+            redirect()->intended(Filament::getUrl());
+        }
+
+        $this->form->fill();
+    }
+
+    public function authenticate(): ?LoginResponse
+    {
+        try {
+            $this->rateLimit(5);
+        } catch (TooManyRequestsException $exception) {
+            $this->addError('email', __('filament::login.messages.throttled', [
+                'seconds' => $exception->secondsUntilAvailable,
+                'minutes' => ceil($exception->secondsUntilAvailable / 60),
+            ]));
+
+            return null;
+        }
+
+        $data = $this->form->getState();
+
+        if (!Filament::auth()->attempt([
+            'email' => $data['email'],
+            'password' => $data['password'],
+        ], $data['remember'])) {
+            $this->addError('email', __('filament::login.messages.failed'));
+
+            return null;
+        }
+
+        return app(LoginResponse::class);
+    }
+
+    protected function getFormSchema(): array
+    {
+        return [
+            TextInput::make('email')
+                ->label(__('filament::login.fields.email.label'))
+                ->email()
+                ->required()
+                ->autocomplete(),
+            TextInput::make('password')
+                ->label(__('filament::login.fields.password.label'))
+                ->password()
+                ->required(),
+            Checkbox::make('remember')
+                ->label(__('filament::login.fields.remember.label')),
+        ];
+    }
+
+    public function render(): View
+    {
+        return view('filament::login')
+            ->layout('filament::components.layouts.card', [
+                'title' => __('filament::login.title'),
+            ]);
+    }
+}
