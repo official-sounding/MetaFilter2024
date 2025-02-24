1ae6907fd445465515517b321bb2583037b1d22c
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-23 18:52:16 -0800

    Added PaginationTrait

---
+++ app/Traits/PaginationTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+
+use Illuminate\Contracts\Pagination\Paginator;
+
+trait PaginationTrait
+{
+    public function data(): Paginator
+    {
+        return $this
+            ->query()
+            ->when($this->orderBy !== '', function ($query) {
+                $query->orderBy(column: $this->orderBy, direction: $this->sortDirection);
+            })
+            ->when($this->searchColumn !== '', function ($query) {
+                $query->whereLike(column: $this->searchColumn, value: trim($this->searchTerm));
+            })
+            ->simplePaginate($this->perPage);
+    }
+
+    public function sort(string $key): void
+    {
+        $this->resetPage();
+
+        if ($this->orderBy === $key) {
+            $this->sortDirection = $this->sortDirection === self::ASCENDING ? self::DESCENDING : self::ASCENDING;
+
+            return;
+        }
+
+        $this->orderBy = $key;
+        $this->sortDirection = self::ASCENDING;
+    }
+}
