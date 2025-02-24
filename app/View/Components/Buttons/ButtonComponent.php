0258a8296051f3b831e79d1175e60e7db4b5c254
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-23 18:53:39 -0800

    Added DTO

--- app/View/Components/Buttons/ButtonComponent.php
+++ app/View/Components/Buttons/ButtonComponent.php
@@ -4,14 +4,23 @@ declare(strict_types=1);
 
 namespace App\View\Components\Buttons;
 
+use App\Dtos\ButtonDto;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 final class ButtonComponent extends Component
 {
-    public function __construct()
+    public string $buttonText = '';
+    public string $class = '';
+    public string $iconFilename = '';
+    public string $type = '';
+
+    public function __construct(ButtonDto $dto)
     {
-        //
+        $this->buttonText = $dto->buttonText;
+        $this->class = $dto->class;
+        $this->iconFilename = $dto->iconFilename;
+        $this->type = $dto->type;
     }
 
     public function render(): View
