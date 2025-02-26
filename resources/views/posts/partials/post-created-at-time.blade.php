bc73adccd53a0f71164b629690ac56e677b12c12
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-26 07:51:44 -0800

    Changed span to time

--- resources/views/posts/partials/post-created-at-time.blade.php
+++ resources/views/posts/partials/post-created-at-time.blade.php
@@ -1,3 +1,3 @@
-<span datetime="{{ $post->created_at->format('Y-m-d H:i:d') }}">
+<time datetime="{{ $post->created_at->format('Y-m-d H:i:d') }}">
     {{ $post->created_at->format('g:i a') }}
-</span>
+</time>
