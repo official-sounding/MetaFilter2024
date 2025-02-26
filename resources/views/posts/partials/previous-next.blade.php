a76fc04306b8252f74d611ec7f308ab0a7e86935
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-25 19:26:58 -0800

    Added conditional check

--- resources/views/posts/partials/previous-next.blade.php
+++ resources/views/posts/partials/previous-next.blade.php
@@ -1,17 +1,19 @@
-<nav class="level previous-next">
-    @if (isset($previous))
-        @include('posts.partials.previous-next-link', [
-            'direction' => 'Previous',
-            'post' => $previous,
-            'routeName' => "$subdomain.post.show",
-        ])
-    @endif
+@if (isset($previous) || isset($next))
+    <nav class="level previous-next">
+        @if (isset($previous))
+            @include('posts.partials.previous-next-link', [
+                'direction' => 'Previous',
+                'post' => $previous,
+                'routeName' => "$subdomain.post.show",
+            ])
+        @endif
 
-    @if (isset($next))
-        @include('posts.partials.previous-next-link', [
-            'direction' => 'Next',
-            'post' => $next,
-            'routeName' => "$subdomain.post.show",
-        ])
-    @endif
-</nav>
+        @if (isset($next))
+            @include('posts.partials.previous-next-link', [
+                'direction' => 'Next',
+                'post' => $next,
+                'routeName' => "$subdomain.post.show",
+            ])
+        @endif
+    </nav>
+@endif
