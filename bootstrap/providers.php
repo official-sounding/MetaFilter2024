53054488d3a224374d13a682801b089a9d47ad64
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-26 08:42:50 -0800

    Added BladeServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Providers\AppServiceProvider;
+use App\Providers\BladeServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
 use App\Providers\FortifyServiceProvider;
 use App\Providers\HorizonServiceProvider;
@@ -13,6 +14,7 @@ use App\Providers\ViewComposerServiceProvider;
 return [
     AdminPanelProvider::class,
     AppServiceProvider::class,
+    BladeServiceProvider::class,
     FortifyServiceProvider::class,
     HorizonServiceProvider::class,
     RepositoryServiceProvider::class,
