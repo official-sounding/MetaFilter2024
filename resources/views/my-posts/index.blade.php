e05de6966e48a8fea030b8573f64685a838c40e8
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-26 07:52:00 -0800

    Added list

--- resources/views/my-posts/index.blade.php
+++ resources/views/my-posts/index.blade.php
@@ -6,13 +6,22 @@
     <h1>{{ $title ?? 'Untitled' }}</h1>
 
     @forelse ($posts as $post)
-        posts
+        @if ($loop->first)
+            <ul>
+        @endif
+            <li>
+                <a href="{{ route('posts.show', $post) }}">
+                    {{ $post->title }}
+                </a>
+            </li>
+        @if ($loop->last)
+            </ul>
+        @endif
     @empty
         @include('notifications.none-listed', [
             'records' => 'draft posts'
         ])
-
-        <x-buttons.new-post-button />
     @endforelse
 
+    <x-buttons.new-post-button />
 @endsection
