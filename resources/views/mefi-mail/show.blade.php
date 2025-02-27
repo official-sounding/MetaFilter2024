9bba031ca304bc25841450885dfe2e1d2f00e384
Kirk Franklin <kirkfranklin@alamedaca.gov>
2025-02-27 12:48:53 -0800

    Added show view

--- resources/views/mefi-mail/show.blade.php
+++ resources/views/mefi-mail/show.blade.php
@@ -3,5 +3,42 @@
 @section('title', $title ?? 'Untitled')
 
 @section('contents')
-    mefi mail show
+    <table>
+        <tbody>
+            <tr>
+                <th scope="row">
+                    {{ trans('From') }}
+                </th>
+                <td>
+                    <address>
+                        <x-members.profile-link-component :user="$mail->sender" />
+                    </address>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">
+                    {{ trans('Date') }}
+                </th>
+                <td>
+                    <x-dates.formatted-date-time-component :date="$mail->created_at" format="g:i a" />
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">
+                    {{ trans('Subject') }}
+                </th>
+                <td>
+                    {{ $mail->subject }}
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">
+                    {{ trans('Message') }}
+                </th>
+                <td>
+                    {{ $mail->message }}
+                </td>
+            </tr>
+        </tbody>
+    </table>
 @endsection
