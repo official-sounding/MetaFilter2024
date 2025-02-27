7a8687a65066cb8bfa7e391906838ff3c5a9a5b2
Kirk Franklin <kirkfranklin@alamedaca.gov>
2025-02-27 15:47:11 -0800

    WIP pagination

--- app/Livewire/Posts/PostIndexComponent.php
+++ app/Livewire/Posts/PostIndexComponent.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace App\Livewire\Posts;
 
+use App\Models\Post;
 use App\Traits\PaginationTrait;
 use App\Traits\PostTrait;
 use App\Traits\SubsiteTrait;
+use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\Paginator;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -21,17 +24,41 @@ final class PostIndexComponent extends Component
 
     protected const string ASCENDING = 'asc';
     protected const string DESCENDING = 'desc';
+    protected const array COLUMNS = [
+        'posts.id',
+        'posts.title',
+        'posts.slug',
+        'posts.link_text',
+        'posts.link_url',
+        'posts.body',
+        'posts.created_at',
+        'posts.deleted_at',
+        'subsites.subdomain AS subdomain',
+        'subsites.name AS subsite',
+        'users.id AS user_id',
+        'users.username',
+    ];
+
+    protected const int PER_PAGE = 20;
 
     public int $page = 1;
     public int $perPage = 20;
     public string $orderBy = 'created_at';
     public string $sortDirection = self::DESCENDING;
-
     public string $subdomain;
+    public mixed $groupedPosts;
+    public mixed $days;
 
     public function boot(): void
     {
         $this->subdomain = $this->getSubdomain();
+
+        $this->days = (new Post())->select(self::COLUMNS)
+            ->orderBy('created_at', self::DESCENDING)
+            ->get()
+            ->groupBy(function ($post) {
+                return Carbon::parse($post->created_at)->format('F j');
+            });
     }
 
     public function render(): View
@@ -43,4 +70,27 @@ final class PostIndexComponent extends Component
     {
         return $this->baseQuery($this->subdomain);
     }
+
+    private function paginate()
+    {
+        // https://jenssegers.com/laravel-pagination-with-grouping-and-eager-loading
+        // https://laracasts.com/discuss/channels/general-discussion/handling-over-7k-records-groupby-and-pagination
+        // https://www.devopsfreelancer.com/blog/how-to-group-a-collection-by-day-month-and-year-in-laravel/
+
+        $results = (new Post())->groupBy(function ($post){
+            return Carbon::parse($post->created_at)->format('F j');
+        })->get();
+
+        $total = count($results);
+        $start = (Paginator::getCurrentPage() - 1) * self::PER_PAGE;
+        $sliced = array_slice((array)$results, $start, self::PER_PAGE);
+
+        $collection = Post::hydrate($sliced);
+
+        $collection->load('relation');
+
+        return Paginator::make($collection->all(), $total, self::PER_PAGE);
+    }
+
+    )
 }
