90c87874f760fa5b19b2ce36c25913348b18d9e4
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-28 09:55:50 -0800

    Added values

--- config/metafilter/navigation/member-sidebar-links.php
+++ config/metafilter/navigation/member-sidebar-links.php
@@ -7,30 +7,30 @@ use App\Enums\RouteNameEnum;
 return [
     [
         'name' => 'Account',
-        'route' => RouteNameEnum::AskPostIndex,
+        'route' => RouteNameEnum::AskPostIndex->value,
     ],
     [
         'name' => 'Contacts',
-        'route' => RouteNameEnum::AskPostIndex,
+        'route' => RouteNameEnum::AskPostIndex->value,
     ],
     [
         'name' => 'Favorites',
-        'route' => RouteNameEnum::AskPostIndex,
+        'route' => RouteNameEnum::AskPostIndex->value,
     ],
     [
         'name' => 'Mail',
-        'route' => RouteNameEnum::AskPostIndex,
+        'route' => RouteNameEnum::AskPostIndex->value,
     ],
     [
         'name' => 'Preferences',
-        'route' => RouteNameEnum::AskPostIndex,
+        'route' => RouteNameEnum::AskPostIndex->value,
     ],
     [
         'name' => 'Profile',
-        'route' => RouteNameEnum::AskPostIndex,
+        'route' => RouteNameEnum::AskPostIndex->value,
     ],
     [
         'name' => 'Recent Activity',
-        'route' => RouteNameEnum::AskPostIndex,
+        'route' => RouteNameEnum::AskPostIndex->value,
     ],
 ];
