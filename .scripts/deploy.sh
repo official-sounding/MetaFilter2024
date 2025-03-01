fc1747b1d856aa2424972b6c034c06fe639ce91a
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-28 18:08:46 -0800

    Added deploy script

---
+++ .scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+echo "Starting deployment..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+(php artisan down --render="errors::maintenance") || true
+
+# Pull the latest version of the app
+git pull origin main
+
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Install npm dependencies
+npm install
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Recreate cache
+php artisan optimize
+
+# Compile npm assets
+npm run build
+
+# Run database migrations
+php artisan migrate --force
+
+# Exit maintenance mode
+php artisan up
+
+echo "Deployment complete"
