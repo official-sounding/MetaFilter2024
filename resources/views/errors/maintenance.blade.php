cb18d52a20349bb4c0f8cf5e0614eb5f52286d37
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-02-28 18:09:46 -0800

    Added maintenance page

---
+++ resources/views/errors/maintenance.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.errors')
+
+@section('title', 'Site Maintenance')
+
+@section('code', '503')
+
+@section('message', 'We are currently performing scheduled maintenance. Please check back soon.')
+
