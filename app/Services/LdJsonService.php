83b8c559189c88f447b9ba0b4e91e4000f60e46f
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-02 14:20:01 -0800

    Added generateAuthorSchema

--- app/Services/LdJsonService.php
+++ app/Services/LdJsonService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Models\Post;
+use App\Models\User;
 use App\Traits\LoggingTrait;
 use DateMalformedStringException;
 use DateTime;
@@ -26,11 +27,13 @@ final class LdJsonService
             $datePublished = new DateTime();
         }
 
+        $author = $post->user()->first();
+
         $blogPosting = Schema::blogPosting()
             ->headline($post->title)
             ->datePublished($datePublished)
             ->dateModified($dateModified)
-            ->author($post->user()->first()->username)
+            ->author($this->generateAuthorSchema($author))
             // TODO: Get canonical URL
 //            ->mainEntityOfPage($post->present()->url)
             ->articleBody($post->body . $post->more_inside);
@@ -46,5 +49,19 @@ final class LdJsonService
             ->url($url);
 
         return $webPage->toScript();
+    }
+
+    protected function generateAuthorSchema(User $user): array
+    {
+        $schema = [
+            '@type' => 'Person',
+            'name' => $user->name ?? '',
+        ];
+
+        if (property_exists($user, 'username')) {
+            $schema['url'] = route('profiles.show', $user->username);
+        }
+
+        return $schema;
     }
 }
