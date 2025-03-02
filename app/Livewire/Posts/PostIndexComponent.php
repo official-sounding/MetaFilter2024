4aa10e218c6cb15fdb7b8290cb508e6b27b3696c
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-01 21:39:48 -0800

    Updated PostIndexComponent

--- app/Livewire/Posts/PostIndexComponent.php
+++ app/Livewire/Posts/PostIndexComponent.php
@@ -8,10 +8,10 @@ use App\Models\Post;
 use App\Traits\PaginationTrait;
 use App\Traits\PostTrait;
 use App\Traits\SubsiteTrait;
-use Carbon\Carbon;
+use Illuminate\Contracts\Pagination\CursorPaginator;
 use Illuminate\Contracts\View\View;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -33,7 +33,7 @@ final class PostIndexComponent extends Component
         'posts.body',
         'posts.created_at',
         'posts.deleted_at',
-        'subsites.subdomain AS subdomain',
+        'subsites.subdomain',
         'subsites.name AS subsite',
         'users.id AS user_id',
         'users.username',
@@ -52,30 +52,68 @@ final class PostIndexComponent extends Component
     public function boot(): void
     {
         $this->subdomain = $this->getSubdomain();
-
-        $this->days = (new Post())->select(self::COLUMNS)
-            ->orderBy('created_at', self::DESCENDING)
-            ->get()
-            ->groupBy(function ($post) {
-                return Carbon::parse($post->created_at)->format('F j');
-            });
     }
 
     public function render(): View
     {
-        return view('livewire.posts.post-index-component');
+        $posts = $this->getPosts();
+
+        return view('livewire.posts.post-index-component', [
+            'posts' => $posts,
+        ]);
     }
 
-    public function query(): Builder
+    private function getPosts(): CursorPaginator
     {
-        return $this->baseQuery($this->subdomain);
+        return Post::select(
+            DB::raw('DATE_FORMAT(posts.created_at, "%m-%d") as month_day'),
+            DB::raw('COUNT(*) as total_posts'),
+            'posts.id',
+            'posts.title',
+            'posts.slug',
+            'posts.body',
+            'posts.user_id',
+            'posts.created_at',
+        )
+        ->join(table: 'users', first: 'posts.user_id', operator: '=', second: 'users.id')
+        ->join(table: 'subsites', first: 'posts.subsite_id', operator: '=', second: 'subsites.id')
+        ->where(column: 'subsites.subdomain', operator: '=', value: $this->subdomain)
+        ->with([
+            'user',
+        ])
+        ->withCount([
+            'comments',
+            'favorites',
+            'flags',
+        ])
+        ->groupBy('month_day', 'id', 'title', 'created_at')
+        ->orderBy(column: 'posts.created_at', direction: self::DESCENDING)
+        ->cursorPaginate(perPage: self::PER_PAGE);
     }
+}
 
-    private function paginate()
-    {
-        // https://jenssegers.com/laravel-pagination-with-grouping-and-eager-loading
-        // https://laracasts.com/discuss/channels/general-discussion/handling-over-7k-records-groupby-and-pagination
-        // https://www.devopsfreelancer.com/blog/how-to-group-a-collection-by-day-month-and-year-in-laravel/
+// https://jenssegers.com/laravel-pagination-with-grouping-and-eager-loading
+// https://laracasts.com/discuss/channels/general-discussion/handling-over-7k-records-groupby-and-pagination
+// https://www.devopsfreelancer.com/blog/how-to-group-a-collection-by-day-month-and-year-in-laravel/
+/*
+ *         $posts = Post::with([
+    'user',
+])
+->withCount([
+    'comments',
+    'favorites',
+    'flags',
+])
+->get()
+->groupBy('created_at')
+->toArray();
+
+$currentPage = LengthAwarePaginator::resolveCurrentPage();
+
+$currentPosts = array_slice($posts, self::PER_PAGE * ($currentPage - 1), self::PER_PAGE);
+
+return new LengthAwarePaginator($currentPosts, count($posts), self::PER_PAGE, $currentPage);
+
 
         $results = (new Post())->groupBy(function ($post){
             return Carbon::parse($post->created_at)->format('F j');
@@ -90,5 +128,20 @@ final class PostIndexComponent extends Component
         $collection->load('relation');
 
         return Paginator::make($collection->all(), $total, self::PER_PAGE);
-    }
-}
+
+        $this->days = (new Post())
+            ->join('users', 'posts.user_id', '=', 'users.id')
+            ->join('subsites', 'posts.subsite_id', '=', 'subsites.id')
+            ->where('subsites.subdomain', '=', $this->subdomain)
+            ->select(self::COLUMNS)
+            ->withCount([
+                'comments',
+                'favorites',
+                'flags',
+            ])
+            ->orderBy('created_at', self::DESCENDING)
+            ->groupBy(function ($post) {
+                return Carbon::parse($post->created_at)->format('F j');
+            })
+            ->get();
+*/
--- resources/views/livewire/posts/post-index-component.blade.php
+++ resources/views/livewire/posts/post-index-component.blade.php
@@ -1,5 +1,16 @@
 <section class="posts">
-    @foreach ($this->data() as $post)
+    @php
+        $previousMonthDay = null;
+    @endphp
+
+    @foreach ($posts as $post)
+        @if ($previousMonthDay !== $post->month_day)
+            <h2>{{ \Carbon\Carbon::createFromFormat('m-d', $post->month_day)->format('F j') }}</h2>
+            @php
+                $previousMonthDay = $post->month_day;
+            @endphp
+        @endif
+
         <livewire:posts.post-index-item-component :post="$post" :key="$post->id" />
     @endforeach
 </section>
