a3aa7d4eedbd2e41fb18bc33ebc2ea1cb64944b4
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-02 14:39:52 -0800

    Check model is Post

--- app/Presenters/PostPresenter.php
+++ app/Presenters/PostPresenter.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Models\Post;
 use App\Traits\PostTrait;
 use Coderflex\LaravelPresenter\Presenter;
+use Illuminate\Database\Eloquent\Model;
+use InvalidArgumentException;
 
 final class PostPresenter extends Presenter
 {
     use PostTrait;
+
+    public function getCanonicalUrl(Model $post): string
+    {
+        if (!($post instanceof Post)) {
+            throw new InvalidArgumentException('Expected Post model');
+        }
+
+        return route('posts.show', $post);
+    }
 
     public function url(): string
     {
