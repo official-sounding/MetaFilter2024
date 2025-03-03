cc440d024f1f31f14a2d4322961e0419dd4da7f6
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-02 17:41:36 -0800

    Send email directly

--- app/Livewire/Wizards/SignupWizardComponent.php
+++ app/Livewire/Wizards/SignupWizardComponent.php
@@ -12,10 +12,12 @@ use App\Http\Requests\Signup\StoreEmailAddressRequest;
 use App\Http\Requests\Signup\StoreOptionalInfoRequest;
 use App\Http\Requests\Signup\StoreUsernameRequest;
 use App\Jobs\SendVerificationEmail;
+use App\Mail\VerifyEmail;
 use App\Models\User;
 use App\Services\UserService;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 final class SignupWizardComponent extends BaseWizardComponent
 {
@@ -115,7 +117,8 @@ final class SignupWizardComponent extends BaseWizardComponent
 
         $this->userService->update($user->id, $data);
 
-        SendVerificationEmail::dispatch($user);
+        //        SendVerificationEmail::dispatch($user);
+        Mail::to($user)->send(new VerifyEmail($user));
 
         Auth::login($user);
 
