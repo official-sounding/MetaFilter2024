a621e88495cc9bca6c30ac02133cf94d3683e87c
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-05 13:55:26 -0800

    RSS setup

--- app/Http/Controllers/RssFeedController.php
+++ app/Http/Controllers/RssFeedController.php
@@ -4,7 +4,27 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Repositories\PostRepositoryInterface;
+use Illuminate\Http\Response;
+
 final class RssFeedController extends BaseController
 {
-    //
+    public function __construct(
+        protected PostRepositoryInterface $postRepository,
+    ) {
+        parent::__construct();
+    }
+
+    public function show(): Response
+    {
+        $posts = Post::latest()->get();
+
+        return response()->view('rss.show', [
+            'posts' => $posts,
+        ])->header(
+            key: 'Content-Type',
+            values: 'text/xml',
+        );
+    }
 }
---
+++ resources/views/rss/show.blade.php
@@ -0,0 +1,38 @@
+@php
+    echo '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
+@endphp
+
+?>
+
+<rss version="2.0">
+    <channel>
+        <title>
+            <![CDATA[ MetaFilter.com ]]>
+        </title>
+        <link>
+            <![CDATA[ https://your-website.com/feed ]]>
+        </link>
+        <description>
+            <![CDATA[ Your website description ]]>
+        </description>
+        <language>
+            en
+        </language>
+        <pubDate>
+            {{ now() }}
+        </pubDate>
+
+        @foreach($posts as $post)
+            <item>
+                <title><![CDATA[{{ $post->title }}]]></title>
+                <link>{{ $post->slug }}</link></link>
+                <description><![CDATA[{!! $post->body !!}]]></description>
+                <category>{{ $post->category }}</category>
+                <author><![CDATA[Hardk Savani]]></author>
+                <guid>{{ $post->id }}</guid>
+                <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
+            </item>
+        @endforeach
+
+    </channel>
+</rss>
