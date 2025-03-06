4ff34d50d3ee2d242fcaf93afef0904cfb40ea6e
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-05 17:40:31 -0800

    Extending BaseFactory

--- database/factories/SubsiteFactory.php
+++ database/factories/SubsiteFactory.php
@@ -5,16 +5,25 @@ declare(strict_types=1);
 namespace Database\Factories;
 
 use App\Models\Subsite;
-use Illuminate\Database\Eloquent\Factories\Factory;
 
-final class SubsiteFactory extends Factory
+final class SubsiteFactory extends BaseFactory
 {
     protected $model = Subsite::class;
 
     public function definition(): array
     {
+        $timestamp = $this->getFakeTimestamp();
+
         return [
-            //
+            'name',
+            'nickname',
+            'tagline',
+            'subdomain',
+            'green_text' => 'Meta',
+            'white_text' => 'Filter',
+            'created_at' => $timestamp,
+            'published_at' => $timestamp,
+            'updated_at' => null,
         ];
     }
 }
