5b58f1a9bf512936727a4c19065b47b0b5f498ba
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-09 16:32:02 -0700

    Code cleanup

--- app/Filament/Resources/PostResource/Pages/EditPost.php
+++ app/Filament/Resources/PostResource/Pages/EditPost.php
@@ -1,19 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
-use Filament\Actions;
+use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
-class EditPost extends EditRecord
+final class EditPost extends EditRecord
 {
     protected static string $resource = PostResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            DeleteAction::make(),
         ];
     }
 }
