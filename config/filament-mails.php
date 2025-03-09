538f5fd5d2e725d974dd7ac1124b5d3fb690f331
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-08 17:06:23 -0800

    Installed vormkracht10/filament-mails

---
+++ config/filament-mails.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Vormkracht10\FilamentMails\Resources\EventResource;
+use Vormkracht10\FilamentMails\Resources\MailResource;
+use Vormkracht10\FilamentMails\Resources\SuppressionResource;
+
+return [
+    'resources' => [
+        'mail' => MailResource::class,
+        'event' => EventResource::class,
+        'suppression' => SuppressionResource::class,
+    ],
+    'navigation' => [
+        'group' => null,
+    ],
+];
