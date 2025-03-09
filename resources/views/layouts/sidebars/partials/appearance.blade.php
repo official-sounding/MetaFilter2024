81cde96d24892b75a2d1d640fc483fa675da4c71
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-08 17:08:22 -0800

    Added toggle-color-scheme-component

--- resources/views/layouts/sidebars/partials/appearance.blade.php
+++ resources/views/layouts/sidebars/partials/appearance.blade.php
@@ -1,8 +1,3 @@
-<x-layout.sidebar-section-component heading="{{  trans('Appearance') }}" open="true">
-    <div>
-        <input type="checkbox" class="theme-toggle" id="theme-toggle">
-        <label for="theme-toggle">
-            Dark mode is <span class="current-theme" id="current-theme">off</span>
-        </label>
-    </div>
+<x-layout.sidebar-section-component heading="{{ trans('Appearance') }}" open="true">
+    <x-buttons.toggle-color-scheme-component />
 </x-layout.sidebar-section-component>
