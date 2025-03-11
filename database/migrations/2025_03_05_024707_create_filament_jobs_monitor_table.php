a9466beca6995142a9f24763a9d1d7358595faa5
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-10 17:08:54 -0700

    Code cleanup

--- database/migrations/2025_03_05_024707_create_filament_jobs_monitor_table.php
+++ database/migrations/2025_03_05_024707_create_filament_jobs_monitor_table.php
@@ -1,13 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('queue_monitors', function (Blueprint $table) {
@@ -23,13 +22,5 @@ return new class extends Migration {
             $table->text('exception_message')->nullable();
             $table->timestamps();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('queue_monitors');
     }
 };
