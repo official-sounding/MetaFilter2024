ead28b2760dc9af709370e45c6eba58b2a626138
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-10 17:45:29 -0700

    Composer rework

--- app/View/Composers/Navigation/FooterSubsiteNavigationViewComposer.php
+++ app/View/Composers/Navigation/FooterSubsiteNavigationViewComposer.php
@@ -16,32 +16,10 @@ final class FooterSubsiteNavigationViewComposer implements ViewComposerInterface
     {
         $navigation = '<ul class="footer-subsite-menu">';
 
-        $items = $this->getItems();
-
-        foreach ($items as $item) {
-            if ($item['in_footer_nav'] === true) {
-                $navigation .= $this->getNavigationItem($item, true);
-            }
-        }
+        $items = config('metafilter.seeders.subsites');
 
         $navigation .= '</ul>';
 
         $view->with('footerSubsiteNavigation', $navigation);
-    }
-
-    private function getItems(): array
-    {
-        $subsites = config('metafilter.seeders.subsites');
-
-        return $this->sortItems($subsites);
-    }
-
-    private function sortItems(array $items): array
-    {
-        usort($items, function ($a, $b) {
-            return $a['footer_navigation_sort_order'] <=> $b['footer_navigation_sort_order'];
-        });
-
-        return $items;
     }
 }
