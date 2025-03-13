62b21985eacd28d0b7263cf55ec4f1ea0742b224
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-13 07:53:53 -0700

    Removed subsite check

--- resources/views/layouts/partials/site-title.blade.php
+++ resources/views/layouts/partials/site-title.blade.php
@@ -1,19 +1,17 @@
-@if (!empty($subsite))
-    <a href="{{ route($subsite['route']) }}" title="Home page">
-        <hgroup class="site-title">
-            @if (empty(request()->segment(1)))
-                <h1 class="site-title-text">
-                    @include('layouts.partials.site-title-text')
-                </h1>
-            @else
-                <p class="site-title-text">
-                    @include('layouts.partials.site-title-text')
-                </p>
-            @endif
+<a href="{{ route($subsite->route) }}" title="Home page">
+    <hgroup class="site-title">
+        @if (empty(request()->segment(1)))
+            <h1 class="site-title-text">
+                @include('layouts.partials.site-title-text')
+            </h1>
+        @else
+            <p class="site-title-text">
+                @include('layouts.partials.site-title-text')
+            </p>
+        @endif
 
-            @if (isset($tagline))
-                <p class="tagline">{{ $tagline }}</p>
-            @endif
-        </hgroup>
-    </a>
-@endif
+        @if (isset($tagline))
+            <p class="tagline">{{ $tagline }}</p>
+        @endif
+    </hgroup>
+</a>
