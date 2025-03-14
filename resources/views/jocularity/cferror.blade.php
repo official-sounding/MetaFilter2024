f9ce67b7435dbe6b42ddf0f241b15acf728aeffb
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-14 15:55:01 -0700

    Added jocularity

---
+++ resources/views/jocularity/cferror.blade.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>CFERROR</title>
+
+<style>
+
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+    padding: 0;
+    border-style: double;
+    border-width: 5px;
+}
+
+html {
+    color: #000;
+    background-color: #ddd;
+    text-size-adjust: none;
+    font-size: 16px;
+    font-family: Verdana, Arial, sans-serif;
+}
+
+main {
+    border-style: double;
+    border-width: 5px;
+    padding: 4px;
+}
+
+p {
+    margin: 0 0 12px 0;
+}
+
+h1 {
+    margin: 0;
+    padding: 4px;
+    color: #fff;
+    background-color: #4646ee;
+    font-size: 21px;
+}
+
+h2 {
+    margin: 0;
+    padding: 4px;
+    color: #fff;
+    background-color: #006;
+    font-size: 21px;
+}
+
+strong {
+    font-size: 1.5rem;
+}
+
+h1,
+h2,
+strong,
+th {
+    font-weight: normal;
+}
+
+</style>
+
+</head>
+<body>
+
+<h2>The following information is meant for the website developer for debugging purposes.</h2>
+
+<h1>Error Occurred While Processing Request</h1>
+
+<main>
+
+<p><strong>The image package is not installed.</strong></p>
+
+<p>You can install package through CLI package manager (C:/ColdFusion/cfusion/bin/cfpm.bat)
+by running the command: install image.</p>
+
+<p>Resources:</p>
+
+<ul>
+    <li>Enable Oscillation Overthruster Information to provide greater detail about the source of errors. In the Administrator, click Debugging & Logging > Debug Output Settings, and select the Robust Exception Information option.</li>
+    <li>Check the ColdFusion documentation to verify that you are using the correct syntax.</li>
+    <li>Pick up the pieces of your weapon, the glaive. Avoid all boulders.</li>
+    <li>Go to your room and think about what you've done.</li>
+    <li>Go outside, turn around three times, and spit.</li>
+</ul>
+
+<table>
+    <tbody>
+    <tr>
+        <th>Browser</th>
+        <td>Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.30</td>
+    </tr>
+    <tr>
+        <th>Remote Address</th>
+        <td>0:0:0:0:0:0:0:1</td>
+    </tr>
+    <tr>
+        <th>Referrer</th>
+        <td></td>
+    </tr>
+    <tr>
+        <th>Date/Time</th>
+        <td>03-Jun-22 01:12 PM</td>
+    </tr>
+    </tbody>
+</table>
+
+</main>
+
+</body>
+</html>
