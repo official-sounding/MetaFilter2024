e6c9da3782949e59379620a2d4963361fcaf1c39
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-14 08:14:06 -0700

    Email updates

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -1,90 +1,121 @@
 <!DOCTYPE html>
-<!--suppress HtmlDeprecatedAttribute -->
-<html lang="{{ app()->getLocale() }}">
+<html lang="en">
 <head>
 
-<meta charset="utf-8">
+<meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!--[if mso]>
-<style>
-    * {
-        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
+<title>{!! $title ?? 'MetaFilter Email' !!}</title>
+
+<!--[if !mso]><!-- -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!--<![endif]-->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<style type="text/css">
+    #outlook a {
+        padding: 0;
+    }
+
+    .ReadMsgBody {
+        width: 100%;
+    }
+
+    .ExternalClass {
+        width: 100%;
+    }
+
+    .ExternalClass * {
+        line-height: 100%;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        -webkit-text-size-adjust: 100%;
+        -ms-text-size-adjust: 100%;
+    }
+
+    table,
+    td {
+        border-collapse: collapse;
+        mso-table-lspace: 0pt;
+        mso-table-rspace: 0pt;
     }
 
 </style>
-<![endif]-->
-
-<!--[if !mso]>
-<link rel="preconnect" href="https://fonts.bunny.net">
-<link rel="stylesheet" href="https://fonts.bunny.net/css?family=montserrat:400,400i">
-<link rel="stylesheet" href="https://fonts.bunny.net/css?family=source-sans-pro:200,200i,300,300i,400,400i,600,600i,700,700i">
-
-<style>
-* {
-    font-family: 'Source Sans Pro', 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
-}
-
-.email-button {
-    display: inline-block;
-    padding: 10px 20px;
-    color: #fff;
-    background-color: #02253b;
-    text-decoration: none;
-    border-radius: 5px;
-    font-weight: bold;
-    transition: background-color 0.3s ease;
-}
-
-.email-button:hover {
-    color: #fff;
-    background-color: #9bc654;
-}
+<!--[if !mso]><!-->
+<style type="text/css">
+    @media only screen and (max-width:480px) {
+        @-ms-viewport {
+            width: 320px;
+        }
+        @viewport {
+            width: 320px;
+        }
+    }
 </style>
-<![endif]-->
+<!--<![endif]-->
 
-<title>{!! $title ?? 'Untitled' !!}</title>
+<!--[if mso]><xml>  <o:OfficeDocumentSettings>    <o:AllowPNG/>    <o:PixelsPerInch>96</o:PixelsPerInch>  </o:OfficeDocumentSettings></xml><![endif]-->
+<!--[if lte mso 11]><style type="text/css">  .outlook-group-fix {    width:100% !important;  }</style><![endif]-->
+
+<!--[if !mso]><!-->
+<link rel="preconnect" href="https://fonts.bunny.net">
+<link rel="stylesheet" href="https://fonts.bunny.net/css?family=source-sans-pro:200,200i,300,300i,400,400i,600,600i,700,700i&amp;display=swap">
+<style type="text/css">
+    @import url('https://fonts.bunny.net/css?family=source-sans-pro:200,200i,300,300i,400,400i,600,600i,700,700i&display=swap');
+</style>
+<!--<![endif]-->
+
+<style type="text/css">
+    @media only screen and (max-width:595px) {
+        .container {
+            width: 100% !important;
+        }
+
+        .button {
+            display: block !important;
+            width: auto !important;
+        }
+    }
+</style>
 
 </head>
-<body>
+<body style="background-color: #aed6e5;">
 
-<table role="presentation" width="100%" cellpadding="0" cellspacing="0" border="0" style="font-size: 100%;font-weight: 400;color: black;background-color: #88c2d8;">
+{{-- TODO: DeEasterEggIfy --}}
+<!-- He's the DJ; I'm the wrapper -->
+<table width="100%" cellspacing="0" cellpadding="0" border="0" align="center">
+    <tbody>
     <tr>
         <!-- He's the DJ; I'm the wrapper -->
-        <td align="center">
-            <table width="600" cellpadding="0" cellspacing="0" border="0">
+        <td valign="top" align="center">
+            <table class="container" width="600" cellspacing="0" cellpadding="0" border="0">
+                <tbody>
                 <tr>
-                    <td style="background-color: #065a8f; font-size: 24px; font-weight: 600; line-height: 1;padding-top: 1rem; padding-bottom: 1rem;">
-                        <span style="color: white;">Meta</span>
-                        <span style="color: #9bc654;">Filter</span>
-                    </td>
+                    <td style="padding: 18px 0 18px 0; text-align: center; font-size: 20px; font-family: 'Source Sans Pro', 'Helvetica Neue', Helvetica, Arial, sans-serif;background-color: #065a8f"><span style="color: white;">Meta</span><span style="color: #9bc654;">Filter</span></td>
                 </tr>
-            </table>
-            <table width="600" cellpadding="0" cellspacing="0" border="0" role="presentation">
                 <tr>
-                    <td style="color: black;background-color: white; font-size: 16px;">
-                        <h1 style="font-family: 'Source Sans Pro', 'Helvetica Neue', Helvetica, Arial, sans-serif;font-weight: 400;">
-                            {!! $title ?? 'Untitled' !!}
-                        </h1>
-                    </td>
-                </tr>
-            </table>
-            <table width="600" cellpadding="0" cellspacing="0" border="0" role="presentation">
-                <tr>
-                    <td style="color: black;background-color: white; font-size: 16px;">
+                    <td class="main-content" style="padding: 24px 16px 24px 16px; color: #000000; font-size: 16px; font-family: 'Source Sans Pro', 'Helvetica Neue', Helvetica, Arial, sans-serif;" bgcolor="#ffffff">
                         @yield('contents')
                     </td>
                 </tr>
-            </table>
-            <table width="600" cellpadding="0" cellspacing="0" border="0" role="presentation">
                 <tr>
-                    <td style="color: #2d2d2d;background-color: #858585; font-size: 10px;">
-                        MetaFilter Community Foundation
+                    <td style="font-size: 0px;">
+                        <!--[if mso | IE]>      <table role="presentation" border="0" cellpadding="0" cellspacing="0">        <tr>          <td style="vertical-align:top;width:300px;">      <![endif]-->
+                        <div class="outlook-group-fix" style="padding: 0 0 20px 0; vertical-align: top; display: inline-block; text-align: center; width:100%;">
+                            <div style="padding: 8px; font-size: 12px; line-height: 16px; font-weight: normal; color: #333; background-color: #c2c2c2; font-family: 'Source Sans Pro', 'Helvetica Neue', Helvetica, Arial, sans-serif;">MetaFilter Community Foundation</span>
+                            </div>
+                            <!--[if mso | IE]>      </td></tr></table>      <![endif]-->
                     </td>
                 </tr>
+                </tbody>
             </table>
         </td>
     </tr>
+    </tbody>
 </table>
 
 </body>
