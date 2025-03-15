f9e62462108fee31c45873e34e91e8cd2f14413f
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-15 10:42:13 -0700

    Time formatting

--- resources/views/comments/partials/comment-timestamp.blade.php
+++ resources/views/comments/partials/comment-timestamp.blade.php
@@ -1,12 +1,11 @@
-<a href="#{{ $comment->id }}" class="footer-info" title="{{ trans('Permanent link to this comment') }}">
-    <x-icons.icon-component filename="clock" />
-    <time
-        datetime="{{ $comment->created_at->format('Y-m-d H:i:d') }}">
+<time datetime="{{ $comment->created_at->format('Y-m-d H:i:d') }}">
+    <a href="#{{ $comment->id }}" class="footer-info" title="{{ trans('Permanent link to this comment') }}">
+        <x-icons.icon-component filename="clock" class="icon-small" />
         {{ $comment->created_at->format('g:i a') }}
-    </time>
-</a>
+    </a>
 
-<span class="footer-info">
-    <x-icons.icon-component filename="calendar3" />
-    {{ $comment->created_at->format('F j') }}
-</span>
+    <span class="footer-info">
+        <x-icons.icon-component filename="calendar3" class="icon-small" />
+        {{ $comment->created_at->format('F j') }}
+    </span>
+</time>
