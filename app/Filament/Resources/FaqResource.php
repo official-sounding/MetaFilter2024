ec4c21dddac04658dbb89be541618523aceb261c
Kirk Franklin <kirkfranklin@alamedaca.gov>
2025-03-17 10:31:40 -0700

    Added FAQ resource

---
+++ app/Filament/Resources/FaqResource.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FaqResource\CreateFaqs;
+use App\Filament\Resources\FaqResource\EditFaqs;
+use App\Filament\Resources\FaqResource\ListFaqs;
+use App\Models\Faq;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+final class FaqResource extends Resource
+{
+    protected static ?string $model = Faq::class;
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public const int INPUT_MAX_LENGTH = 255;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->required()
+                    ->maxLength(self::INPUT_MAX_LENGTH),
+                TextInput::make('url')
+                    ->required()
+                    ->maxLength(self::INPUT_MAX_LENGTH),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('question')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                EditAction::make(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListFaqs::route('/'),
+            'create' => CreateFaqs::route('/create'),
+            'edit' => EditFaqs::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/FaqResource/CreateFaqs.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\FaqResource;
+
+use App\Filament\Resources\FaqResource;
+use Filament\Resources\Pages\CreateRecord;
+
+final class CreateFaqs extends CreateRecord
+{
+    protected static string $resource = FaqResource::class;
+}
