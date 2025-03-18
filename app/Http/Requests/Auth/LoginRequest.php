945e9e6410f91f5cfab1e7db621444d5e7e565ab
Kirk Franklin <kirkfranklin@alamedaca.gov>
2025-03-18 16:54:22 -0700

    Added properties

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -6,15 +6,21 @@ namespace App\Http\Requests\Auth;
 
 use App\Http\Requests\BaseFormRequest;
 use App\Traits\AuthStatusTrait;
+use App\Traits\LoggingTrait;
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 
+/**
+ * @property string $password
+ * @property string $username
+ */
 final class LoginRequest extends BaseFormRequest
 {
     use AuthStatusTrait;
+    use LoggingTrait;
 
     public function authorize(): bool
     {
@@ -24,32 +30,42 @@ final class LoginRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
-            'username' => [
+            'password' => [
                 'required',
                 'string',
             ],
-            'password' => [
+            'username' => [
                 'required',
                 'string',
             ],
         ];
     }
 
+    /**
+     * @throws ValidationException
+     */
     public function authenticate(): void
     {
-        $this->ensureIsNotRateLimited();
+        try {
+            $this->ensureIsNotRateLimited();
+        } catch (ValidationException $exception) {
+            $this->logError($exception);
+        }
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if (! Auth::attempt($this->only('username', 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+                'username' => trans('auth.failed'),
             ]);
         }
 
         RateLimiter::clear($this->throttleKey());
     }
 
+    /**
+     * @throws ValidationException
+     */
     public function ensureIsNotRateLimited(): void
     {
         if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
