43f76134cf20541e76f4fe3ce8819eb368a08f3e
Kirk Franklin <kirkfranklin@alamedaca.gov>
2025-03-19 12:55:38 -0700

    Added site ID and published_at

--- app/Http/Controllers/Api/v1/PostController.php
+++ app/Http/Controllers/Api/v1/PostController.php
@@ -8,37 +8,41 @@ use App\Dtos\PostDto;
 use App\Enums\PostStateEnum;
 use App\Http\Requests\Post\StorePostRequest;
 use App\Services\PostService;
+use App\Traits\SubsiteTrait;
 use Illuminate\Http\JsonResponse;
 
 final class PostController extends BaseApiController
 {
+    use SubsiteTrait;
+
     public function __construct(protected PostService $postService) {}
 
     public function store(StorePostRequest $request): JsonResponse
     {
         $validated = $request->validated();
 
-        $timestamp = now()->toDateTimeString();
-
-        // TODO: Get the site ID
+        $subsiteId = $this->getSubsiteId();
 
         $dto = new PostDto(
             title: $validated['title'],
             body: $validated['content'],
             more_inside: $validated['more_inside'],
             user_id: $validated['user_id'],
-            subsite_id: 1,
+            subsite_id: $subsiteId,
             state: PostStateEnum::Published->value,
-            published_at: $timestamp,
+            published_at: now()->toDateTimeString(),
             is_published: true,
         );
 
         $post = $this->postService->store($dto);
 
         $message = 'Post created';
+
         // TODO: Get the post URL
+        $postUrl = '';
+
         $data = [
-            'post_url' => '',
+            'post_url' => $postUrl,
         ];
 
         return $this->sendResponse($message, $data);
