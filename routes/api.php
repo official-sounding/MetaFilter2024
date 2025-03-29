9edb8784210e53822852333cde19c923a3b23377
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-03-29 10:35:46 -0700

    Added auth:api

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'prefix' => 'api/v1/',
-    'middleware' => ['auth:sanctum'],
 ], function () {
     Route::post('login', [LoginController::class, 'store']);
-    Route::post('posts', [PostController::class, 'store']);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('posts', [PostController::class, 'store']);
+    });
 });
