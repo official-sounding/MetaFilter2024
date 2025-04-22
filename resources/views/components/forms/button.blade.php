2a5175d3edfb3513159b897ec4665e171782a1e9
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-04-22 16:53:17 -0700

    Added onclick

--- resources/views/components/forms/button.blade.php
+++ resources/views/components/forms/button.blade.php
@@ -1,5 +1,13 @@
+@props([
+    'type' => 'button',
+    'onclick' => null,
+])
+
 <x-forms.field>
     <button
+        @if ($onclick !== null)
+            wire:click="{{ $onclick }}"
+        @endif
         type="{{ $type ?? 'button' }}"
         class="button @if (isset($class)) {{ $class }} @endif"
         wire:loading.attr="disabled">
