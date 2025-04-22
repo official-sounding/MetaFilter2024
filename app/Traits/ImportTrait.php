4666c1f729897e94a6707cb5b51b8274914a6671
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-04-22 16:53:07 -0700

    Added prepareDatabaseForImport

--- app/Traits/ImportTrait.php
+++ app/Traits/ImportTrait.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Traits;
 
 use App\Dtos\ImportDto;
+use Illuminate\Support\Facades\DB;
+use RuntimeException;
 
 trait ImportTrait
 {
@@ -18,6 +20,24 @@ trait ImportTrait
             ->onlyLoadColumns($dto->columns)
             ->setColumn($dto->columnLabel, $dto->columnValue)
             ->load();
+    }
+
+    public function prepareDatabaseForImport(): void
+    {
+        DB::statement('SET @@session.unique_checks = 0');
+        DB::statement('SET @@session.foreign_key_checks = 0');
+    }
+
+    public function openFileFromStorage(string $filePath)
+    {
+        if (!file_exists(storage_path($filePath))) {
+            throw new RuntimeException("File not found: $filePath");
         }
+
+        if (!is_readable(storage_path($filePath))) {
+            throw new RuntimeException("File is not readable: $filePath");
+        }
+
+        return fopen(storage_path($filePath), 'r');
     }
 }
