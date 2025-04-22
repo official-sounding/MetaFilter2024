8e5582f2a095faad135574c590edc72cb4fd32aa
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-04-22 16:54:38 -0700

    Added input components

--- resources/views/livewire/wizards/signup/steps/02-password-step.blade.php
+++ resources/views/livewire/wizards/signup/steps/02-password-step.blade.php
@@ -3,20 +3,26 @@
     @include('forms.partials.csrf-token')
 
     <fieldset>
-        <livewire:auth.input-password-component
-            name="password"
-            label="Next, enter a password"
+        <x-forms.input
+            :name="'password'"
+            :type="'password'"
+            :label="'Next, pick your password'"
+            :autofocus="true"
+            :required="true"
         />
-
-        <livewire:auth.input-password-component
-            name="password_confirmation"
-            label="And confirm your password"
+        <x-forms.input
+            :name="'password_confirmation'"
+            :type="'password'"
+            :label="'And confirm your password'"
+            :autofocus="false"
+            :required="true"
         />
     </fieldset>
 
     <div class="level">
         <x-forms.button
             class="secondary-button previous-step"
+            onclick="goToStep(1)"
             type="button">
             {{ trans('Previous') }}
         </x-forms.button>
