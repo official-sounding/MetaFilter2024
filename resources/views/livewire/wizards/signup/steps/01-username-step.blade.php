23becd3ad0946987d8f0b2bb2f1ed1b224af94b2
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-04-22 18:12:09 -0700

    Added name

--- resources/views/livewire/wizards/signup/steps/01-username-step.blade.php
+++ resources/views/livewire/wizards/signup/steps/01-username-step.blade.php
@@ -10,6 +10,14 @@
                     Usernames may consist of English letters, numbers, and most punctuation.'"
             :label="'First, pick your username'"
             :autofocus="true"
+            :required="true"
+        />
+
+        <x-forms.input
+            :name="'name'"
+            :type="'text'"
+            :label="'What&rsquo;s your name?'"
+            :autofocus="false"
             :required="false"
         />
     </fieldset>
