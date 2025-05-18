af9478d6f37ecb7a6aa22775db21550e7a010819
Kirk Franklin <3633070+kirkaracha@users.noreply.github.com>
2025-05-18 09:57:20 -0700

    Added createUser

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
+use App\Dtos\UserDto;
 use App\Models\User;
 
 final class UserRepository extends BaseRepository implements UserRepositoryInterface
@@ -11,6 +12,17 @@ final class UserRepository extends BaseRepository implements UserRepositoryInter
     public function __construct(User $model)
     {
         parent::__construct($model);
+    }
+
+    public function createUser(UserDto $dto): User
+    {
+        return User::create([
+            'username' => $dto->username,
+            'name' => $this->name ?? null,
+            'email' => $dto->email,
+            'password' => $dto->password,
+            'state' => $dto->state,
+        ]);
     }
 
     public function updateState(User $user, string $state): void
--- app/Repositories/UserRepositoryInterface.php
+++ app/Repositories/UserRepositoryInterface.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
+use App\Dtos\UserDto;
 use App\Models\User;
 
 interface UserRepositoryInterface extends BaseRepositoryInterface
 {
+    public function createUser(UserDto $dto);
+
     public function updateState(User $user, string $state);
 }
