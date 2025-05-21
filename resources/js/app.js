dbcc37f23f222e5c2eba0ed9506a244fce294898
Peter Elliott <pelliott@official-sounding.com>
2025-05-20 21:46:01 -0500

    update theme toggle js to be more resilient

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,8 @@ import './bootstrap';
 import Clipboard from '@ryangjchandler/alpine-clipboard';
 import Precognition from 'laravel-precognition-alpine';
 
-import {closeDropdown, toggleDropdowns} from './modules/toggleDropdowns.js';
-import {toggleTheme} from './modules/toggleTheme.js';
+import { toggleDropdowns } from "./modules/toggleDropdowns.js";
+import { registerThemeToggle } from "./modules/toggleTheme.js";
 
 window.Alpine = Alpine;
 
@@ -14,79 +14,12 @@ Alpine.plugin([
 ]);
 
 toggleDropdowns();
-toggleTheme();
+registerThemeToggle();
 
-document.addEventListener('click', event => {
-    let eventTarget = event.target;
-/*
-    if (event.key === 'Escape'){
-        //do something here
-        Livewire.dispatch('escape-key-clicked');
-    }
-*/
-    if (!eventTarget.closest('.global-navigation-menu')) {
-//        closeDropdown('global-navigation-dropdown-toggle');
-    }
-}, false);
-
-document.addEventListener('livewire:init', () => {
-    Livewire.on('onkeydown', (event) => {
-        if (event.key === 'Escape') {
-            Livewire.dispatch('escape-key-clicked', false);
+document.addEventListener("livewire:init", () => {
+    Livewire.on("onkeydown", (event) => {
+        if (event.key === "Escape") {
+            Livewire.dispatch("escape-key-clicked", false);
         }
     });
 });
-
-/*
-function keyPress (e)(){
-document.addEventListener("keyup", keyPress);
-    <script>
-        document.onkeydown = function(event) {
-            if (event.key === 'Escape') {
-                window.livewire.emit('cancelReplying');
-            }
-        };
-    </script>
-
-document.addEventListener('click', function (event) {
-    if (!event.target.closest('.some-selector')) {
-        // Clicked outside the element...
-    }
-}, false);
-      click on dropdown toggle
-      add id of dropdown to list of open dropdowns
-      add event listener to document
-        if click is outside of dropdown
-          remove id of dropdown from list of open dropdowns
-
-
-
-    let globalNavigationExpanded = getExpanded(globalNavigationToggle);
-    let utilityNavigationExpanded = false;
-
-    if (utilityNavigationToggle !== null) {
-        utilityNavigationExpanded = getExpanded(utilityNavigationToggle);
-    }
-    if (eventTarget === darkModeToggle) {
-        toggleDarkMode(eventTarget);
-    }
-
-    do {
-        if (eventTarget === globalNavigationToggle || eventTarget === utilityNavigationToggle) {
-            console.log('Clicked inside!');
-            toggleDropdown(eventTarget);
-        }
-
-        eventTarget = eventTarget.parentNode;
-    } while (eventTarget);
-
-    if (globalNavigationExpanded) {
-        toggleDropdown(globalNavigationToggle);
-    }
-
-    if (utilityNavigationExpanded) {
-        toggleDropdown(utilityNavigationToggle);
-    }
-*/
-
-//    console.log('Clicked outside!');
